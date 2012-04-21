38178b3b60f4f4560078b598e45ef41c6dff1804
Thom van Kalkeren <thomvankalkeren@gmail.com>
2012-04-21 13:46:51 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenDebate::Application.config.secret_token = '76f31f24b98c0fc9db60b7909da8564114b592ba64e52f10984ff8e9fd0264dcf269f3822ae02b1a939474ecae836e27d32d1e7331effdc33a099d46de4e5ec4'
