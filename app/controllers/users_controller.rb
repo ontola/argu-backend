b544d5df36dc2613057c7c86cb543f587e6055e1
Thom van Kalkeren <thomvankalkeren@gmail.com>
2012-08-30 23:40:57 +0200

    removed debug puts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,11 +48,6 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-      
-      if request.xhr?
-        puts "==============test============================"
-      end
-
       respond_to do |format|
         format.js #{ render partital: 'form' }
         format.html
