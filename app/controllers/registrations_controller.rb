3a81c7c90a04d1845511b4eed5d7fb1a792547c3
Thom van Kalkeren <thomvankalkeren@gmail.com>
2012-09-02 19:19:08 +0200

    EOD commit

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,25 @@ class RegistrationsController < Devise::RegistrationsController
 		session[:omniauth] = nil unless @user.new_record?
 	end
 
+	def edit
+      @user = User.find(current_user.id)
+      email_changed = @user.email != params[:email]
+      password_changed = @user.encrypted_password.blank? ? false : !params[:password].empty? #if the pass's NULL, the user signed up via an auth service
+      successfully_updated = if email_changed or password_changed
+        @user.update_with_password(params[:user])
+      else
+        @user.update_without_password(params[:user])
+      end
+
+      if successfully_updated
+        # Sign in the user bypassing validation in case his password changed
+        sign_in @user, :bypass => true
+        redirect_to root_path
+      else
+        render "edit"
+      end
+	end
+
 	def cancel
 		unless current_user.nil?
 			render 'cancel'
