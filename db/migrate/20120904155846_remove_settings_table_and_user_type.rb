41bdbaae2d43fde66df65cbd481334384b47bddb
Thom van Kalkeren <thomvankalkeren@gmail.com>
2012-09-05 13:41:51 +0200

    Added abilties and migrations

---
+++ db/migrate/20120904155846_remove_settings_table_and_user_type.rb
@@ -0,0 +1,16 @@
+class RemoveSettingsTableAndUserType < ActiveRecord::Migration
+  def up
+  	drop_table :settings
+  end
+
+  def down
+  	create_table :settings, :force => true do |t|
+    	t.string  :var,         :null => false
+    	t.text    :value
+    	t.integer :target_id
+    	t.string  :target_type, :limit => 30
+    	t.timestamps
+  	end
+    add_column :users, :clearance, :int
+  end
+end
