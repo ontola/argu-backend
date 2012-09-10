1ecb9f44ebf404720ea3117cdc7ae34051cfe369
Thom van Kalkeren <thomvankalkeren@gmail.com>
2012-09-10 11:20:41 +0200

    Updated versions table to refuse null for the object field

--- db/schema.rb
+++ db/schema.rb
@@ -110,8 +110,8 @@ ActiveRecord::Schema.define(version: 20120905133430) do
     t.string   "item_type",                     null: false
     t.integer  "item_id",                       null: false
     t.string   "event",                         null: false
-    t.string   "whodunnit"
-    t.text     "object"
+    t.string   "whodunnit",                     null: false
+    t.text     "object",                        null: false
     t.datetime "created_at"
   end
   add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
