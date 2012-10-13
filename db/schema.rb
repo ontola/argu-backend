6eb3be6662f6c7ef591989f46f44b90f2b1437a3
joepsz <joepmeindertsma@gmail.com>
2012-10-13 15:10:49 +0200

    Fixed CSS info page and footer

--- db/schema.rb
+++ db/schema.rb
@@ -11,119 +11,80 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20120905133430) do
+ActiveRecord::Schema.define(:version => 20120807205349) do
 
-  create_table "arguments", force: true do |t|
-    t.string   "content",                       null: false
-    t.integer  "argtype",                       default: 3, null: false
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+  create_table "arguments", :force => true do |t|
+    t.string   "content",                   :null => false
+    t.integer  "argtype",    :default => 3, :null => false
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
     t.string   "title"
   end
 
-  create_table "authentications", force: true do |t|
-    t.integer  "user_id",                       null: false
-    t.string   "provider",                      null: false
-    t.string   "uid",                           null: false
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-  end
-  add_index "authentications", ["user_id"], name: "user_id"
-  add_index "authentications", ["user_id", "uid"], name: "user_id_and_uid", unique: true
-
-  create_table "comments", force: true do |t|
-    t.integer  "commentable_id",                default: 0
-    t.string   "commentable_type",              default: ""
-    t.string   "title",                         default: ""
-    t.text     "body",                          default: ""
-    t.string   "subject",                       default: ""
-    t.integer  "user_id",                       default: 0,  null: false
+  create_table "comments", :force => true do |t|
+    t.integer  "commentable_id",   :default => 0
+    t.string   "commentable_type", :default => ""
+    t.string   "title",            :default => ""
+    t.text     "body",             :default => ""
+    t.string   "subject",          :default => ""
+    t.integer  "user_id",          :default => 0,  :null => false
     t.integer  "parent_id"
     t.integer  "lft"
     t.integer  "rgt"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-  end
-  add_index "comments", ["commentable_id"], name: "index_comments_on_commentable_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
-
-  create_table "profiles", force: true do |t|
-    t.integer  "user_id"#,                       null: false
-    t.string   "name",                          default: ""
-    t.text     "about",                         default: ""
-    t.string   "picture",                       default: ""
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-  end
-  add_index "profiles", ['user_id'], name: "profiles_by_user_id", unique: true
-
-  create_table "roles", force: true do |t|
-    t.string   "name",                          null: false
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.datetime "created_at",                       :null => false
+    t.datetime "updated_at",                       :null => false
   end
 
-  create_table "roles_users", id: false, force: true do |t|
-    t.integer "role_id",                        null: false
-    t.integer "user_id",                        null: false
-  end
-  add_index "roles_users", ["user_id", "role_id"], name: "user_role", unique: true
+  add_index "comments", ["commentable_id"], :name => "index_comments_on_commentable_id"
+  add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
 
-
-  create_table "statementarguments", force: true do |t|
-    t.integer "argument_id",                    null: false
-    t.integer "statement_id",                   null: false
-    t.boolean "pro",                            default: true, null: false
-    t.integer "votes_count",                    default: 0
-  end
-  add_index "statementarguments", ["argument_id", "statement_id"], name: "arg_state_index"
-
-  create_table "statements", force: true do |t|
-    t.string   "title",                         null: false
-    t.string   "content",                       null: false
-    t.integer  "statetype",                     default: 6
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+  create_table "settings", :force => true do |t|
+    t.string   "var",                       :null => false
+    t.text     "value"
+    t.integer  "target_id"
+    t.string   "target_type", :limit => 30
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
   end
 
-  create_table "users", force: true do |t|
-    t.string   "email",                         default: ""
-    t.string   "encrypted_password",            default: ""
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                 default: 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+  add_index "settings", ["target_type", "target_id", "var"], :name => "index_settings_on_target_type_and_target_id_and_var", :unique => true
+
+  create_table "statementarguments", :force => true do |t|
+    t.integer "argument_id",                    :null => false
+    t.integer "statement_id",                   :null => false
+    t.boolean "pro",          :default => true, :null => false
+    t.integer "votes_count",  :default => 0
+  end
+
+  create_table "statements", :force => true do |t|
+    t.string   "title",                     :null => false
+    t.string   "content",                   :null => false
+    t.integer  "statetype",  :default => 6
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+  end
+
+  create_table "users", :force => true do |t|
     t.string   "username"
-    t.string   "unconfirmed_email"
+    t.string   "email",                          :null => false
+    t.string   "name"
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
+    t.string   "password_digest"
+    t.string   "remember_token"
+    t.integer  "clearance",       :default => 4
   end
-  add_index "users", ["email"], name: "index_users_on_email", where: "email IS NOT NULL", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
-  create_table "versions", force: true do |t|
-    t.string   "item_type",                     null: false
-    t.integer  "item_id",                       null: false
-    t.string   "event",                         null: false
-    t.string   "whodunnit",                     null: false
-    t.text     "object",                        null: false
-    t.datetime "created_at"
-  end
-  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
-  create_table "votes", force: true do |t|
-    t.integer  "statementargument_id",          null: false
-    t.integer  "user_id",                       null: false
+  create_table "votes", :force => true do |t|
+    t.integer  "statementargument_id", :null => false
+    t.integer  "user_id",              :null => false
     t.integer  "vote_type"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.datetime "created_at",           :null => false
+    t.datetime "updated_at",           :null => false
   end
-  add_index "votes", ["statementargument_id", "user_id"], name: "index_votes_on_statementargument_id_and_user_id", unique: true
+
+  add_index "votes", ["statementargument_id", "user_id"], :name => "index_votes_on_statementargument_id_and_user_id", :unique => true
 
 end
-  
