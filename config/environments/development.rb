50e36c08fcd1ae240e123055e4b3a310e9601434
Joep Meindertsma <joepmeindertsma@gmail.com>
2013-01-07 21:57:10 +0100

    Updated arguments and profile translations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,4 +42,8 @@ Argu::Application.configure do
   config.assets.debug = true
 
   config.i18n.available_locales = :nl
+
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation can not be found)
+  config.i18n.fallbacks = true
 end
