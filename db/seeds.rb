f50543cd8d8e72e2f538a528b52905e083676135
Thom van Kalkeren <thomvankalkeren@gmail.com>
2013-07-20 14:49:49 +0200

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-
-['user', 'moderator', 'admin', 'coder'].each do |role|
-  Role.find_or_create_by_name(role).save
-end
-
-ua = User.create(email: 'thomvankalkeren@gmail.com', username: 'fletcher91', password: 'foobar', password_confirmation:'foobar' );
-ub = User.create(username: 'admin', email: 'postmaster@argu.nl', password:'opendebate', password_confirmation:'opendebate')
-uc = User.create(email: 'joepmeindertsma@gmail.com', username: 'joep', password: 'joepjoep', password_confirmation:'joepjoep' );
+ua = User.create(email: 'thomvankalkeren@gmail.com', username: 'fletcher91', password: 'foobar', password_confirmation:'foobar', role: :coder);
+ub = User.create(username: 'admin', email: 'postmaster@argu.nl', password:'opendebate', password_confirmation:'opendebate', role: :admin)
+uc = User.create(email: 'joepmeindertsma@gmail.com', username: 'joep', password: 'joepjoep', password_confirmation:'joepjoep', role: :admin);
 
 ua.profile = Profile.create(name: 'Thom van Kalkeren', picture: 'http://www.wthex.com/images/coolcookie.gif', about: "I'm the coder!")
 ub.profile = Profile.create(name: 'Administrator')
 uc.profile = Profile.create(name: 'Joep', picture: 'https://lh5.googleusercontent.com/-fgiBDzie7Jk/UEoCv42lzzI/AAAAAAAABZk/nfYf52duV4o/s518/profielfoto.jpg', about: "argu designer")
-
-ua.roles << Role.find_by_name('coder')
-ub.roles << Role.find_by_name('admin')
-uc.roles << Role.find_by_name('user')
