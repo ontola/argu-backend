2735466694094b7d7ebf6ca20ac552f42df545f1
Joep Meindertsma <joepmeindertsma@gmail.com>
2014-01-05 20:13:36 +0100

    added '.no-touch' class to classes that use :hover

--- app/assets/javascripts/touch.js
+++ app/assets/javascripts/touch.js
@@ -1,3 +1,5 @@
-if (!("ontouchstart" in document.documentElement)){ 
-    document.documentElement.className += " no-touch"; 
+//test for touch events support and if not supported, attach .no-touch class to the HTML tag.
+ 
+if (!("ontouchstart" in document.documentElement)) {
+document.documentElement.className += " no-touch";
 }
