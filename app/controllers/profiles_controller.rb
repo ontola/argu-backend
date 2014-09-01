3363e748416f0eb05bac050f88a9d7c777b56934
Thom van Kalkeren <thomvankalkeren@gmail.com>
2014-09-02 00:23:02 +0200

    [FIX] Some fixes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,8 @@
 class ProfilesController < ApplicationController
-	load_and_authorize_resource
 
 	#GET /profiles/1
 	def show
+    @profile = Profile.find params[:id]
 		@user = User.find_by_id(@profile.user_id)
 
 		respond_to do |format|
@@ -12,6 +12,8 @@ class ProfilesController < ApplicationController
 
 	#GET /1/edit
 	def edit
+    @current_user = current_user.profile
+
 		respond_to do |format|
 			format.html # edit.html.erb
 		end
@@ -19,6 +21,7 @@ class ProfilesController < ApplicationController
 
 	#PUT /1
 	def update
+    @profile = current_user.profile
 		respond_to do |format|
 			if @profile.update_attributes(params[:profile])
 				format.html { redirect_to @profile, notice: "Profile was successfully updated." }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Argu::Application.configure do
   # config.assets.manifest = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
+  config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
   
   # See everything in the log (default is :info)
