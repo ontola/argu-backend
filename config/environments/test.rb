c23e3abe3872f00b23dd2a122b7d8d11b0f2e7f9
Thom van Kalkeren <thomvankalkeren@gmail.com>
2014-10-06 12:23:17 +0200

    store fix

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,7 +28,8 @@ Argu::Application.configure do
   # Disable request forgery protection in test environment
   config.action_controller.allow_forgery_protection    = false
 
-  config.session_store :active_record_store, key: '_Argu_session', domain: 'logos.argu.nl'
+  config.session_store :cookie_store, key: '_Argu_session', domain: 'logos.argu.nl'
+  #config.session_store :active_record_store, key: '_Argu_session', domain: 'logos.argu.nl'
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
