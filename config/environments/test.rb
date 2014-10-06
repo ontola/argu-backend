e45cd91fec1c57db1a1d332987e1893e1c061e81
Thom van Kalkeren <thomvankalkeren@gmail.com>
2014-10-06 12:26:50 +0200

    no message

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,11 @@
 Argu::Application.configure do
+  config.host = "argu.nl"
   # Settings specified here will take precedence over those in config/application.rb
+
+  config.epics = ActiveSupport::OrderedOptions.new
+  config.epics.opinion_buttons = false
+  config.epics.opinion = true
+  config.epics.parties = false
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
@@ -10,7 +16,7 @@ Argu::Application.configure do
   config.eager_load = false
 
   # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_assets = true
+  config.serve_static_assets = false
   config.static_cache_control = "public, max-age=3600"
 
   # Log error messages when you accidentally call methods on nil
@@ -19,6 +25,9 @@ Argu::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
 
   config.log_level = :debug
 
@@ -38,4 +47,6 @@ Argu::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.i18n.available_locales = [:nl, :en]
 end
