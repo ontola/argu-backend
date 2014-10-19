b59fa977f59cc5e3e80eedf836206788c82a708d
Thom van Kalkeren <thomvankalkeren@gmail.com>
2014-10-19 09:33:13 +0200

    [FIX] Only count non-trashed arguments

--- app/models/argument.rb
+++ app/models/argument.rb
@@ -6,7 +6,7 @@ class Argument < ActiveRecord::Base
   scope :argument_comments, -> { includes(:comment_threads).where(is_trashed: false).order(votes_pro_count: :desc).references(:comment_threads) }
 
   counter_culture :statement,
-                  column_name: Proc.new { |a| "argument_#{a.pro? ? 'pro' : 'con'}_count" },
+                  column_name: Proc.new { |a| a.is_trashed ? nil : "argument_#{a.pro? ? 'pro' : 'con'}_count" },
                   column_names: {
                       ["pro = ?", true] => 'argument_pro_count',
                       ["pro = ?", false] => 'argument_con_count'
