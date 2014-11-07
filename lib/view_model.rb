85ed7b21e4631ca0f91731e19094e05c55e8d798
Fletcher91 <thomvankalkeren@gmail.com>
2014-11-07 16:06:03 +0100

    [FIX] org#show statements fix

--- lib/view_model.rb
+++ lib/view_model.rb
@@ -1,6 +1,10 @@
 # Monkey patches the Cell::ViewModel class to return false on #is_haml?
 # This is done since we're using slim (apparently #is_haml? isn't part of the Tilt API)
-
+module ViewModel
+  def is_haml?
+    false
+  end
+end
 module Cell
   class ViewModel
     def is_haml?
