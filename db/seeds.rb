4b32ebb66ed4aa43719fec06579444c9a718c699
Fletcher91 <thomvankalkeren@gmail.com>
2014-11-18 12:30:45 +0100

    [FEATURE] Display version in staff_sign [FIX] If this doesn't fix database seed error on semaphore, they're just whiny about nothing

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,22 +2,27 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 ua = User.where(email: 'thomvankalkeren@gmail.com').first_or_create
-ua.update_attributes(username: 'fletcher91', password: 'foobar', password_confirmation:'foobar')
 ub = User.where(email: 'postmaster@argu.nl').first_or_create
-ub.update_attributes(username: 'admin', password:'opendebate', password_confirmation:'opendebate')
 uc = User.where(email: 'joepmeindertsma@gmail.com').first_or_create
+
+ua.update_attributes(username: 'fletcher91', password: 'foobar', password_confirmation:'foobar')
+ub.update_attributes(username: 'admin', password:'opendebate', password_confirmation:'opendebate')
 uc.update_attributes(username: 'joep', password: 'joepjoep', password_confirmation:'joepjoep')
 
 
-pa = Profile.where(name: 'Thom van Kalkeren')
-pa.first_or_create.update_attributes(picture: 'http://www.wthex.com/images/coolcookie.gif', about: "I'm the coder!")
-ua.profile = pa
-
+pa = Profile.where(name: 'Thom van Kalkeren').first_or_create
 pb = Profile.where(name: 'Administrator').first_or_create
-ub.profile = pb
+pc = Profile.where(name: 'Joep').first_or_create
 
-pc = Profile.where(name: 'Joep')
-pc.first_or_create.update_attributes(picture: 'https://lh5.googleusercontent.com/-fgiBDzie7Jk/UEoCv42lzzI/AAAAAAAABZk/nfYf52duV4o/s518/profielfoto.jpg', about: "argu designer")
+pa.update_attributes(picture: 'http://www.wthex.com/images/coolcookie.gif', about: "I'm the coder!")
+pc.update_attributes(picture: 'https://lh5.googleusercontent.com/-fgiBDzie7Jk/UEoCv42lzzI/AAAAAAAABZk/nfYf52duV4o/s518/profielfoto.jpg', about: "argu designer")
+
+pa = pa.first if pa.respond_to? :first
+pb = pb.first if pb.respond_to? :first
+pc = pc.first if pc.respond_to? :first
+
+ua.profile = pa
+ub.profile = pb
 uc.profile = pc
 
 pa.add_role :coder
