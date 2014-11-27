e1241f60bb10f8710c15d9bb535988cb842853b5
Fletcher91 <thomvankalkeren@gmail.com>
2014-11-27 19:00:24 +0100

    [FIX] Parentable now takes multiple options, goes up the chain until an association is found

--- app/models/concerns/parentable.rb
+++ app/models/concerns/parentable.rb
@@ -7,12 +7,24 @@ module Parentable
   # Gives the context of an object based on the parameters (if needed/any)
   # @return Hash with :model pointing to the model or collection, and :url as the visitable url
   def get_parent(options={})
+    if parent_is.class == Symbol
+      parent = reflect_parent(parent_is, options)
+    elsif parent_is.class == Array
+      parent_is.each do |_parent|
+        parent = reflect_parent(_parent, options)
+        parent || break
+      end
+    end
+    parent || Context.new
+  end
+
+  def reflect_parent(relation_name, options)
     parent = Context.new
-    if self.class.reflect_on_association(parent_is).macro == :belongs_to
-      parent.model = send(parent_is)
+    if self.class.reflect_on_association(relation_name).macro == :belongs_to
+      parent.model = send(relation_name)
     else
       begin
-        parent.model = send(parent_is).find(options["#{parent_is.to_s.singularize}_id"])
+        parent.model = send(relation_name).find(options["#{relation_name.to_s.singularize}_id"])
       rescue ActiveRecord::RecordNotFound
         parent.model = nil
       end
@@ -21,7 +33,7 @@ module Parentable
   end
 
   module ClassMethods
-    def parentable(relation)
+    def parentable(*relation)
       cattr_accessor :parent_is do
         relation
       end
