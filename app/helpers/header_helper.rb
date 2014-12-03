7188b87dbaa74343741ad747d0b93b48a2f5a7e7
joepsz <joepmeindertsma@gmail.com>
2014-12-03 14:06:12 +0100

    [FEATURE] shortened text in home button for forum

--- app/helpers/header_helper.rb
+++ app/helpers/header_helper.rb
@@ -2,6 +2,6 @@ module HeaderHelper
 
   # Label for the home button
   def home_text
-    current_scope.model.try(:display_name) || t("home_title")
+    current_scope.model.try(:display_name)[0..13] + (current_scope.model.try(:display_name)[14] ? "..." : "") || t("home_title")
   end
 end
