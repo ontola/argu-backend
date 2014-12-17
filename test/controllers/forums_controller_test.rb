4ec53078e83714f5907d381e0abb007678be5109
Fletcher91 <thomvankalkeren@gmail.com>
2014-12-17 15:44:25 +0100

    [TEST] Basic forum functional test

---
+++ test/controllers/forums_controller_test.rb
@@ -0,0 +1,35 @@
+require "test_helper"
+
+class ForumsControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
+  test "should get show" do
+    sign_in users(:user)
+
+    get :show, id: forums(:utrecht)
+    assert_response :success
+    assert_not_nil assigns(:forum)
+  end
+
+  test "should not show settings" do
+    sign_in users(:user)
+
+    get :settings, id: forums(:utrecht)
+    assert_redirected_to root_path
+  end
+
+  test "should not show statistics" do
+    sign_in users(:user)
+
+    get :settings, id: forums(:utrecht)
+    assert_redirected_to root_path
+  end
+
+  test "should not put update on others question" do
+    sign_in users(:user)
+
+    put :update, id: forums(:utrecht), question: {title: 'New title', content: 'new contents'}
+    assert_redirected_to root_path
+  end
+
+end
