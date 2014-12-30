8a4ef7dfefa3b97b1f2d85918b5696fd6daa73a6
Fletcher91 <thomvankalkeren@gmail.com>
2014-12-30 14:21:05 +0100

    [IMP] No error screen in production anymore

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Argu::Application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
