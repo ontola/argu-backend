c032a3e7862bf96f861ba87c5125507f51a75c08
Fletcher91 <thomvankalkeren@gmail.com>
2014-12-30 12:55:50 +0100

    [IMP] Deprecated serve_static_assets to ~static_files

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -16,7 +16,7 @@ Argu::Application.configure do
   config.eager_load = false
 
   # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_assets = false
+  config.serve_static_files = false
   config.static_cache_control = "public, max-age=3600"
 
   # Log error messages when you accidentally call methods on nil
