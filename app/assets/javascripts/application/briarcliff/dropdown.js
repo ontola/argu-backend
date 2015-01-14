735dc71917c14ae1327865f688a9154e6abcc9d7
joepsz <joepmeindertsma@gmail.com>
2015-01-14 18:11:25 +0100

    [IMP] Carrierwave center images, higher res icons [FEATURE] Show icons in profile switcher [FIX] Prevent Default action on first dropdown item (for ios and android)

--- app/assets/javascripts/application/briarcliff/dropdown.js
+++ app/assets/javascripts/application/briarcliff/dropdown.js
@@ -4,13 +4,15 @@ $(function() {
 
     //Toggle dropdown content when clicked on trigger
     _dropdown.on("tap click", function (e){
-        // Prevents dropdown-active from opening the neighboring link in Chrome for android.. but also prevents clicking on dropdown content!
-        //e.preventDefault();
-
         $(this).toggleClass("dropdown-active");
     });
 
-    //Remove dropdown content when clicked anywhere else
+    _dropdown.find("div:first").on("tap click", function (e) {
+        // Prevents dropdown-active from opening the neighboring link in Chrome for android.. but also prevents clicking on dropdown content!
+        e.preventDefault();
+    });
+
+        //Remove dropdown content when clicked anywhere else
     $('body').on("tap click", function(event){
         _dropdown.not(_dropdown.has(event.target)).removeClass("dropdown-active");
     });
