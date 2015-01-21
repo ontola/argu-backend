7df0229df7373dcc31826fdfd421ad7d1fcc34f6
joepsz <joepmeindertsma@gmail.com>
2015-01-21 18:53:30 +0100

    [IMP] Translations, descriptions

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -15,11 +15,11 @@ class VotesController < ApplicationController
       elsif (@vote = Vote.find_or_create_by(voteable: @model, voter: current_profile, forum: @model.forum)).update(for: params[:for])
         @model.reload
         save_vote_to_stats @vote
-        format.html { redirect_to @model, notice: '_Successfully voted_' }
+        format.html { redirect_to @model, notice: t('votes.alerts.success') }
         format.js
         format.json { render json: @vote, status: :created, location: @vote }
       else
-        format.html { redirect_to @model, notice: '_Vote failed_' }
+        format.html { redirect_to @model, notice: t('votes.alerts.failed') }
         format.js { head :bad_request }
         format.json { render json: @vote.errors, status: :bad_request }
       end
