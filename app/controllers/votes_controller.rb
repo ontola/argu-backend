fdfbb4cdb5d65ee795148fbe8c233f98c99da5a0
Fletcher91 <thomvankalkeren@gmail.com>
2015-01-30 18:16:23 +0100

    [FIX] Votes are now access token creditable

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,29 +6,34 @@ class VotesController < ApplicationController
     elsif params[:motion_id].present?
       @model = Motion.find params[:motion_id]
     end
-    authorize @model.forum, :show?
+    if current_profile.blank?
+      authorize @model, :show?
+      render_register_modal(nil)
+    else
+      authorize @model.forum, :show?
 
-    @vote = Vote.find_or_initialize_by(voteable: @model, voter: current_profile, forum: @model.forum)
+      @vote = Vote.find_or_initialize_by(voteable: @model, voter: current_profile, forum: @model.forum)
 
-    respond_to do |format|
-      if !current_profile.member_of? @model.forum
-        format.js { render partial: 'forums/join', layout: false, locals: { forum: @model.forum, r: request.fullpath } }
-        format.html { render template: 'forums/join', locals: { forum: @model.forum, r: request.fullpath } }
-      elsif @vote.for == params[:for]
-        format.html { redirect_to @model, notice: t('votes.alerts.not_modified') }
-        format.js { head :not_modified }
-        format.json { render json: @vote, status: :not_modified }
-      elsif @vote.update(for: params[:for])
-        create_activity @vote, action: :create, parameters: {for: @vote.for}, recipient: @vote.voteable, owner: current_profile, forum_id: @vote.forum.id
-        @model.reload
-        save_vote_to_stats @vote
-        format.html { redirect_to @model, notice: t('votes.alerts.success') }
-        format.js
-        format.json { render json: @vote, status: :created, location: @vote }
-      else
-        format.html { redirect_to @model, notice: t('votes.alerts.failed') }
-        format.js { head :bad_request }
-        format.json { render json: @vote.errors, status: :bad_request }
+      respond_to do |format|
+        if !current_profile.member_of? @model.forum
+          format.js { render partial: 'forums/join', layout: false, locals: { forum: @model.forum, r: request.fullpath } }
+          format.html { render template: 'forums/join', locals: { forum: @model.forum, r: request.fullpath } }
+        elsif @vote.for == params[:for]
+          format.html { redirect_to @model, notice: t('votes.alerts.not_modified') }
+          format.js { head :not_modified }
+          format.json { render json: @vote, status: :not_modified }
+        elsif @vote.update(for: params[:for])
+          create_activity @vote, action: :create, parameters: {for: @vote.for}, recipient: @vote.voteable, owner: current_profile, forum_id: @vote.forum.id
+          @model.reload
+          save_vote_to_stats @vote
+          format.html { redirect_to @model, notice: t('votes.alerts.success') }
+          format.js
+          format.json { render json: @vote, status: :created, location: @vote }
+        else
+          format.html { redirect_to @model, notice: t('votes.alerts.failed') }
+          format.js { head :bad_request }
+          format.json { render json: @vote.errors, status: :bad_request }
+        end
       end
     end
   end
