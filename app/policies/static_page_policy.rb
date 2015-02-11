4669c1401d1c0684d4c17cc966045b839cc78c4f
joepsz <joepmeindertsma@gmail.com>
2015-02-11 23:34:34 +0100

    [IMP] Added tests for json block documents

--- app/policies/static_page_policy.rb
+++ app/policies/static_page_policy.rb
@@ -1,4 +1,16 @@
 class StaticPagePolicy < Struct.new(:user, :static_pages)
+  attr_reader :context, :user, :record, :session
+
+  def initialize(context, record)
+    @context = context
+    @record = record
+
+    #raise Argu::NotLoggedInError.new(nil, record), "must be logged in" unless has_access_to_platform?
+  end
+
+  delegate :user, to: :context
+  delegate :session, to: :context
+
   def home?
     true
   end
@@ -8,7 +20,7 @@ class StaticPagePolicy < Struct.new(:user, :static_pages)
   end
 
   def product?
-    @user && @user.profile.has_role?(:staff)
+    user && user.profile.has_role?(:staff)
   end
 
   def sign_in_modal?
