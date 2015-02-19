8e6ce174894f682fe95c9e2ec3ad1f7080985820
Fletcher91 <thomvankalkeren@gmail.com>
2015-02-19 18:26:45 +0100

    [IMP] Meagre improvement to url handling in references

--- app/helpers/arguments_helper.rb
+++ app/helpers/arguments_helper.rb
@@ -20,7 +20,7 @@ private
           if ref[0].blank?
             concat content_tag :li, content_tag(:p, ref[1], id: ref[2])
           else
-            concat content_tag :li, link_to(ref[1].present? ? ref[1] : ref[0], '//' + ref[0], id: ref[2])
+            concat content_tag :li, link_to(ref[1].present? ? ref[1] : ref[0], ref[0], id: ref[2], target: '_blank')
           end
         end
       end
--- app/models/concerns/has_references.rb
+++ app/models/concerns/has_references.rb
@@ -8,12 +8,12 @@ module HasReferences
   #TODO escape content=(text)
   def supped_content
     refs = 0
-    content.gsub(/(\[[\w\\\/\:\?\&\%\_\=\.\+\-\,\#]*\])(\([\w\s!@#\$%^&*,.<>?|\\\/]*\))/) {|url,text| '<a class="reference-inline" href="%s#ref%d">%d</a>' % [Rails.application.routes.url_helpers.argument_path(self), refs += 1, refs] }
+    content.gsub(/(\[[\w\\\/\:\?\(\)\&\%\_\=\.\+\-\,\#]*\])(\([\w\s!@#\$%^&*,.<>?|\(\)\\\/]*\))/) {|url,text| '<a class="reference-inline" href="%s#ref%d">%d</a>' % [Rails.application.routes.url_helpers.argument_path(self), refs += 1, refs] }
   end
 
   def references
     refs = 0
-    content.scan(/\[([\w\\\/\:\?\&\%\_\=\.\+\-\,\#]*)\]\(([\w\s!@#\$%^&*,.<>?|\\\/]*)\)/).each { |r| r << 'ref' + (refs += 1).to_s }
+    content.scan(/\[([\w\\\/\:\?\(\)\&\%\_\=\.\+\-\,\#]*)\]\(([\w\s!@#\$%^&*,.<>?\(\)|\\\/]*)\)/).each { |r| r << 'ref' + (refs += 1).to_s }
   end
 
   module ClassMethods
