50cf1f4c039ec389a29207f5f68fe02fce8ac8fa
Fletcher91 <thomvankalkeren@gmail.com>
2015-03-08 15:22:28 +0100

    [FIX] Show divider for people not in the notifications pool

--- app/helpers/forums_helper.rb
+++ app/helpers/forums_helper.rb
@@ -9,14 +9,16 @@ module ForumsHelper
       link_items << link_item(membership.forum.display_name, url_for(membership.forum), image: membership.forum.profile_photo.url(:icon))
     end
 
+    divided = false
     if active_for_user?(:notifications, current_user)
+      divided = true
       if current_profile.following?(@forum)
         link_items << link_item(t('forums.unfollow'), follows_path(forum_id: @forum.web_url), fa: 'times', divider: 'top', data: {method: 'delete', 'skip-pjax' => 'true'})
       else
         link_items << link_item(t('forums.follow'), follows_path(forum_id: @forum.web_url), fa: 'check', divider: 'top', data: {method: 'create', 'skip-pjax' => 'true'})
       end
     end
-    link_items << link_item(t('forums.leave'), forum_membership_path(@forum.web_url, current_profile), fa: 'sign-out', data: {method: :delete, 'skip-pjax' => 'true', confirm: t('forums.leave_confirmation')}) if policy(@forum).is_member?
+    link_items << link_item(t('forums.leave'), forum_membership_path(@forum.web_url, current_profile), fa: 'sign-out', divider: (divided ? 'top' : nil), data: {method: :delete, 'skip-pjax' => 'true', confirm: t('forums.leave_confirmation')}) if policy(@forum).is_member?
     link_items << link_item(t('forums.discover'), forums_url, fa: 'compass') if policy(@forum).is_member?
     dropdown_options(resource.name, [{items: link_items}], triggerTag: 'h1', fa_after: 'fa-angle-down')
   end
