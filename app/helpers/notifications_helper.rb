e1de89b1d9ec15cffbe9ad99d3cfd72d858de405
Fletcher91 <thomvankalkeren@gmail.com>
2015-03-09 11:17:58 +0100

    [IMP] Improved notification message texts

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,6 +2,10 @@ module NotificationsHelper
 
   def notifications_for(user, page)
     _notifications = policy_scope(user.profile.notifications).includes(activity: :trackable).order(created_at: :desc).page(page)
-    _notifications.map { |n| {id: n.id, title: n.activity.trackable.display_name, url: url_for(n.activity.trackable), read: n.read_at.present?, created_at: n.created_at} }
+    _notifications.map do |n|
+      parent = (n.activity.trackable.try(:parent) && n.activity.trackable.parent) || n.activity.recipient
+      title = "#{n.activity.owner.display_name} #{t("activities.#{n.activity.trackable.class_name}.create#{'_your' if parent.creator == current_user.profile}", thing: t("#{parent.class_name}.type"))}"
+      { id: n.id, title: title, url: url_for(n.activity.trackable), read: n.read_at.present?, created_at: n.created_at }
+    end
   end
 end
--- app/views/notifications/index.json.jbuilder
+++ app/views/notifications/index.json.jbuilder
@@ -3,7 +3,8 @@ json.notifications do
   json.lastNotification @notifications.first && @notifications.first.created_at
   json.notifications @notifications do |notification|
     json.id notification.id
-    json.title notification.activity.trackable.display_name
+    parent = (notification.activity.trackable.try(:parent) && notification.activity.trackable.parent) || notification.activity.recipient
+    json.title "#{notification.activity.owner.display_name} #{t("activities.#{notification.activity.trackable.class_name}.create#{'_your' if parent.creator == current_user.profile}", thing: t("#{parent.class_name}.type"))}"
     json.url url_for(notification.activity.trackable)
     json.read notification.read_at.present?
     json.read_at notification.read_at
