cf52d3e37d869fbcdf2a57170d047c77a00b1ef7
joepsz <joepmeindertsma@gmail.com>
2015-03-26 15:16:20 +0100

    [IMP] Modal closeable with ESC

--- app/assets/javascripts/application/briarcliff/modal.js
+++ app/assets/javascripts/application/briarcliff/modal.js
@@ -1,21 +1,6 @@
 $(function () {
     "use strict";
 
-    function modalClose (){
-        var container = $(this).parent('.modal-container');
-
-        container.addClass('modal-hide');
-        window.setTimeout(function () {
-            container.remove();
-        }, 500);
-    }
-
-    document.addEventListener('keyup', function(e) {
-        if (e.keyCode == 27) {
-            modalClose();
-        }
-    });
-
     $(document).on('click', '.modal-container .overlay:not(.no-close)', function () {
         var container = $(this).parent('.modal-container');
 
@@ -23,6 +8,15 @@ $(function () {
         window.setTimeout(function () {
             container.remove();
         }, 500);
+    });
+
+    document.addEventListener('keyup', function(e) {
+        if (e.keyCode == 27) {
+            $('.modal-container').addClass('modal-hide');
+            window.setTimeout(function () {
+                $('.modal-container').remove();
+            }, 500);
+        }
     });
 
 });
