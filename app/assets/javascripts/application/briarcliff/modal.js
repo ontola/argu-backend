41014b840dae2adcbee002b63fb1faca2dfc1198
joepsz <joepmeindertsma@gmail.com>
2015-03-26 14:58:30 +0100

    [IMP] Modal hover feedback

--- app/assets/javascripts/application/briarcliff/modal.js
+++ app/assets/javascripts/application/briarcliff/modal.js
@@ -1,5 +1,21 @@
 $(function () {
     "use strict";
+
+    function modalClose (){
+        var container = $(this).parent('.modal-container');
+
+        container.addClass('modal-hide');
+        window.setTimeout(function () {
+            container.remove();
+        }, 500);
+    }
+
+    document.addEventListener('keyup', function(e) {
+        if (e.keyCode == 27) {
+            modalClose();
+        }
+    });
+
     $(document).on('click', '.modal-container .overlay:not(.no-close)', function () {
         var container = $(this).parent('.modal-container');
 
