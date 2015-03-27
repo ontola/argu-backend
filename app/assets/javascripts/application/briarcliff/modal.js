bfc15ae875ad55330b7ca4b9a16191310c48b3cf
joepsz <joepmeindertsma@gmail.com>
2015-03-27 12:14:08 +0100

    [IMP] Modal & overlay close css

--- app/assets/javascripts/application/briarcliff/modal.js
+++ app/assets/javascripts/application/briarcliff/modal.js
@@ -2,7 +2,7 @@ $(function () {
     "use strict";
 
     //Close modal when clicking on overlay
-    $(document).on('click', '.modal-container .overlay:not(.no-close)', function () {
+    $(document).on('click', '.modal-container:not(.no-close) .overlay', function () {
         var container = $(this).parent('.modal-container');
 
         container.addClass('modal-hide');
