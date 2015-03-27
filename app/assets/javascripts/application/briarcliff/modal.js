4b8018f5aed28d101b62a674a3bf837ce2c84d5a
joepsz <joepmeindertsma@gmail.com>
2015-03-27 13:29:49 +0100

    [FIX] Modal closing with esc on no-close.

--- app/assets/javascripts/application/briarcliff/modal.js
+++ app/assets/javascripts/application/briarcliff/modal.js
@@ -1,7 +1,7 @@
 $(function () {
     "use strict";
 
-    //Close modal when clicking on overlay
+    //Close modal when clicking on overlay, unless there is a "no-close" class.
     $(document).on('click', '.modal-container:not(.no-close) .overlay', function () {
         var container = $(this).parent('.modal-container');
 
@@ -11,9 +11,9 @@ $(function () {
         }, 500);
     });
 
-    //Close modal when pressing escape button
+    //Close modal when pressing escape button, unless there is a "no-close" class.
     document.addEventListener('keyup', function(e) {
-        if (e.keyCode == 27) {
+        if (e.keyCode == 27 && !$(".no-close")[0]) {
             $('.modal-container').addClass('modal-hide');
             window.setTimeout(function () {
                 $('.modal-container').remove();
