ba309c84eb2353ce7516ec1f19c9ff69046a4aa1
joepsz <joepmeindertsma@gmail.com>
2015-03-27 17:21:02 +0100

    [IMP] Added profile edit button to navbar dropdown, show manage bar in user profiles

--- app/helpers/header_helper.rb
+++ app/helpers/header_helper.rb
@@ -48,6 +48,7 @@ module HeaderHelper
           {
               items: [
                   link_item(t('profiles.display'), dual_profile_path(current_profile), fa: 'user'),
+                  link_item(t('profiles.edit.title'), dual_profile_edit_path(current_profile), fa: 'pencil'),
                   link_item(t('users_show_title'), settings_url, fa: 'gear'),
                   link_item(t('sign_out'), destroy_user_session_url, fa: 'sign-out', data: {method: 'delete', 'skip-pjax' => 'true'})
               ]
