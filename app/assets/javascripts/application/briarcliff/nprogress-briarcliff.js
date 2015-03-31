c49d24058b119a848a07b9a1acb4e77c75b939d0
joepsz <joepmeindertsma@gmail.com>
2015-03-31 12:03:14 +0200

    [IMP] Added nprogress for ajax events, also works in closed environment.

--- app/assets/javascripts/application/briarcliff/nprogress-briarcliff.js
+++ app/assets/javascripts/application/briarcliff/nprogress-briarcliff.js
@@ -1,3 +1,7 @@
 $(document).on('pjax:start',   function() { NProgress.start(); });
 $(document).on('pjax:success',  function() { NProgress.done(); });
 $(document).on('pjax:end', function() { NProgress.remove(); });
+
+$(document).on('ajax:beforeSend',   function() { NProgress.start(); });
+$(document).on('ajax:complete',  function() { NProgress.done(); });
+$(document).on('ajax:after', function() { NProgress.remove(); });
