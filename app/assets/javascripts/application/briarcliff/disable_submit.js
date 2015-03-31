b6fb5e3521bb8bd089613e447915ff05865a4408
joepsz <joepmeindertsma@gmail.com>
2015-03-31 13:47:40 +0200

    [IMP] Button disable not working properly

--- app/assets/javascripts/application/briarcliff/disable_submit.js
+++ app/assets/javascripts/application/briarcliff/disable_submit.js
@@ -1,4 +1,28 @@
-$('button:submit').click(function(){
-    $('button:submit').addClass("is-loading");
-    //The class should be removed again on pageload and if there are errors.
+$(document).on('ready pjax:success', function(){
+
+    $('button:submit').click(function(){
+        $('button:submit').addClass("is-loading")
+        setTimeout(function(){
+            $('button:submit').removeClass("is-loading");
+        },2500);
+    });
+
+    // Inspired by http://www.alfajango.com/blog/rails-3-remote-links-and-forms/
+    // Adds a class to buttons with the class 'remote-link'
+    $('.remote-link')
+        .bind("ajax:beforeSend", function(evt, xhr, settings){
+            var $submitButton = $(this);
+            $submitButton.addClass("is-loading");
+
+        })
+        .bind('ajax:complete', function(evt, xhr, status){
+            var $submitButton = $(this);
+            $submitButton.removeClass("is-loading");
+        })
+        .bind("ajax:error", function(evt, xhr, status, error){
+
+            var $submitButton = $(this);
+            $submitButton.removeClass("is-loading");
+        });
+
 });
