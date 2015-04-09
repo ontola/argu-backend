1a73d3ac8d40a9f2c036eeb0169c206d879de48b
joepsz <joepmeindertsma@gmail.com>
2015-04-09 14:50:17 +0200

    [IMP] Markdown in lots of views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -121,4 +121,22 @@ module ApplicationHelper
     end
   end
 
+
+  # TODO: Something something instance variable with Redcarpet
+  def markdown_to_html(markdown)
+    Redcarpet::Markdown.new(
+        Redcarpet::Render::HTML.new(filter_html: false, escape_html: true),
+        {tables: false, fenced_code_blocks: false, no_styles: true, escape_html: true, autolink: true, lax_spacing: true}
+    ).render(markdown).html_safe
+  end
+
+  def markdown_to_plaintext(markdown)
+    require 'redcarpet/render_strip'
+
+    Redcarpet::Markdown.new(
+        Redcarpet::Render::StripDown.new,
+        {tables: false, fenced_code_blocks: false, no_styles: true, escape_html: true, autolink: false, lax_spacing: true}
+    ).render(markdown)
+  end
+
 end
