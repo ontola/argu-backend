379113364321e27313a478838c3bcbae66e41961
joepsz <joepmeindertsma@gmail.com>
2015-04-23 14:11:20 +0200

    [FIX] Static pages tests.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,20 +3,18 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   include Devise::TestHelpers
 
-  test 'should get team' do
+  test 'should get redirect' do
     sign_in users(:user)
 
     get :team
 
-    assert_response :success
-    assert assigns(:document)
-    assert_equal 'block--full', assigns(:document)['sections'].first['type']
+    assert_response 302
   end
 
-  test 'should get product' do
+  test 'should get how_argu_works' do
     sign_in users(:user)
 
-    get :product
+    get :how_argu_works
 
     assert_response :success
   end
