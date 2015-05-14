c5c0ba398df63a836edc4c12ca45df2593e912d5
Fletcher91 <thomvankalkeren@gmail.com>
2015-05-14 14:42:03 +0200

    [IMP] Change menu to use information_controller

--- app/helpers/header_helper.rb
+++ app/helpers/header_helper.rb
@@ -104,10 +104,10 @@ module HeaderHelper
         sections: [
           {
               items: [
-                  link_item(t('about.vision'), about_path),
-                  link_item(t('about.team'), team_path  ),
-                  link_item(t('about.governments'), governments_path),
-                  link_item(t('about.how_argu_works'), how_argu_works_path),
+                  link_item(t('about.vision'), info_path(:about)),
+                  link_item(t('about.team'), info_path(:team_path  )),
+                  link_item(t('about.governments'), info_path(:governments_path)),
+                  link_item(t('about.how_argu_works'), info_path(:how_argu_works_path)),
                   link_item(t('intro.start'), nil, className: 'intro-trigger', data: {:'skip-pjax' => true})
               ]
           }
