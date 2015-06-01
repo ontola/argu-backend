79134996b7b50e1e9571e98217f53eeaa860c339
Fletcher91 <thomvankalkeren@gmail.com>
2015-06-01 10:50:44 +0200

    [FIX] Set default locale so that the assets pipeline picks it up

--- config/application.rb
+++ config/application.rb
@@ -59,5 +59,7 @@ module Argu
     config.i18n.load_path += Dir["#{Rails.root.to_s}/config/locales/**/*.{rb,yml}"]
     config.i18n.enforce_available_locales = true
     I18n.enforce_available_locales = true
+    config.i18n.default_locale = :nl
+    I18n.locale = :nl
   end
 end
