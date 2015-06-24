1aed9251c1bc57bbd367767d053871acb20b67c2
Fletcher91 <thomvankalkeren@gmail.com>
2015-06-24 09:49:21 +0200

    [SEC] Rules now require a Context to be defined (otherwise, the logic gets too complicated)

--- lib/exception_to_the_rule.rb
+++ lib/exception_to_the_rule.rb
@@ -2,7 +2,7 @@ module ExceptionToTheRule
   TRICKLE_LOGIC = { 'doesnt_trickle' => :==, 'trickles_down' => :<=, 'trickles_up' => :>= }
 
   def apply_rules(action, level)
-    rules = find_rules_for_level(action, level)
+    rules = context.context_model.present? ? find_rules_for_level(action, level) : []
     rules.presence ? rules.map { |r| r.permit ? level : nil }.compact.presence : level
   end
 
@@ -10,14 +10,15 @@ module ExceptionToTheRule
     rules.find_all { |r| level.send(TRICKLE_LOGIC[r.trickles], self.send(r.role)) } if level
   end
 
+  # Waarschijnlijk een context_type nil toevoegen aan het eerste query gedeelte.
   def find_rules_for_level(action, level)
     _rules = Rule.arel_table
-    rule_query = (_rules[:model_type].eq(@record.is_a?(Class) ? @record.to_s : @record.class.to_s)
-                 .and(_rules[:model_id].eq(@record.try(:id))
-                 .and(_rules[:action].eq(action.to_s)))
-                 ).or(_rules[:action].eq(action.to_s)
-                 .and(_rules[:context_type].eq(context.context_model.class.to_s))
-                 .and(_rules[:context_id].eq(context.context_model.try(:id).to_s)))
+    rule_query = _rules[:model_type].eq(@record.is_a?(Class) ? @record.to_s : @record.class.to_s)
+                   .and(_rules[:model_id].eq(@record.try(:id))
+                   .and(_rules[:action].eq(action.to_s))
+                   .and(_rules[:context_type].eq(context.context_model.class.to_s))
+                   .and(_rules[:context_id].eq(context.context_model.id.to_s))
+                 )
     rules = Rule.where(rule_query)
     filter_trickle(rules, level)
   end
