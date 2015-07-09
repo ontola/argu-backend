85a1b7d78e0d86bd6f4cfcae6618298eb0e295ce
Fletcher91 <thomvankalkeren@gmail.com>
2015-07-09 11:18:47 +0200

    [IMP] Add UA to logstash info

--- config/initializers/logstasher.rb
+++ config/initializers/logstasher.rb
@@ -7,6 +7,7 @@ if LogStasher.enabled
     fields[:user_id] = current_user && current_user.id
     fields[:profile] = current_profile && current_profile.url
     fields[:profile_id] = current_profile && current_profile.id
+    fields[:ua] = request.env['HTTP_USER_AGENT']
     fields[:a_params] = request.try(:params).try(:slice, 'at', 'r', 'q')
     if (cs = current_context.context_scope(current_profile)).present?
       fields[:forum] = cs.model.url
