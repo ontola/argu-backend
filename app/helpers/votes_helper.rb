8dfc8e81608c3a3279d05e5a211f9acf57f2e92f
Fletcher91 <thomvankalkeren@gmail.com>
2015-07-17 17:30:38 +0200

    [IMP] Added `data-voted-on` attribute to _shr voting button

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -3,9 +3,12 @@ module VotesHelper
   def toggle_vote_link(model, vote, &block)
     url = vote.try(:persisted?) ? vote_path(vote) : polymorphic_url([model, :vote], for: :pro)
     data = {remote: true, method: :post, title: t('tooltips.argument.vote_up')}
-    data[:method] = :delete if vote.present?
+    if vote.present?
+      data[:method] = :delete
+      data['voted-on'] = true
+    end
 
-    link_to url, rel: :nofollow, class: "btn-subtle tooltip tooltip--left #{'btn-subtle-active' if vote.present?}", data: data do
+    link_to url, rel: :nofollow, class: 'btn-subtle tooltip tooltip--left', data: data do
       yield
     end
   end
