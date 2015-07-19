2c5fb2c00f883310b38d2f63b7bd89b619bdc6fe
Fletcher91 <thomvankalkeren@gmail.com>
2015-07-20 01:33:39 +0200

    [IMP] Take GroupMemberships along in Group destruction

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ActiveRecord::Base
   include ArguBase
 
   belongs_to :forum
-  has_many :group_memberships
+  has_many :group_memberships, dependent: :destroy
   has_many :members, through: :group_memberships, class_name: 'Profile'
   has_many :group_responses
 
