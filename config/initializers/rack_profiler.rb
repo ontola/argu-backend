66a46a647d33a1f90fc44dac1c10945ea53a2bc4
Joep Meindertsma <joepmeindertsma@gmail.com>
2015-09-08 13:33:40 +0200

    [IMP] Dropdown uses width of content [FIX] Drodown creates unused space in dom [IMP] Profiler is hidden by default

--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -1,6 +1,7 @@
 if Rails.env.development? || Rails.env.staging?
   require 'rack-mini-profiler'
 
+  Rack::MiniProfiler.config.start_hidden = true
   # initialization is skipped so trigger it
   Rack::MiniProfilerRails.initialize!(Rails.application)
 end
