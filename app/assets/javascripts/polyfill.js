2d89b418f89c7f9d8b83b70f7a61a5f59986f1bb
Fletcher91 <thomvankalkeren@gmail.com>
2015-09-16 00:15:06 +0200

    [IMP] JSON polyfill

--- app/assets/javascripts/polyfill.js
+++ app/assets/javascripts/polyfill.js
@@ -10,6 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require polyfills/json3
 //= require js-polyfills/es5
 //= require js-polyfills/es6
 //= require js-polyfills/html
