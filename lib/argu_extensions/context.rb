51c80e998a94790f68d3841aca02f8f426b9f1d1
Thom Van Kalkeren <thomvankalkeren@gmail.com>
2015-09-16 13:36:29 +0000

    [REVERT] Didn't work

--- lib/argu_extensions/context.rb
+++ lib/argu_extensions/context.rb
@@ -2,7 +2,7 @@ module ArguExtensions
   module Context
     # Descends down the context tree until a forum is found.
     def context_scope(current_profile, default_nil = false)
-      val = if self.present?
+      if self.present?
         if self.model.class == Forum
           if current_profile && current_profile.memberships.pluck(:forum_id).include?(self.model.id)
             Argu::Redis.set("profile:#{current_profile.id}:last_forum", self.model.id)
@@ -18,8 +18,6 @@ module ArguExtensions
           ::Context.new current_profile.present? ? current_profile.preferred_forum : Forum.first_public
         end
       end
-      Argu::Redis.set("profile:#{current_profile.id}:last_forum", val.root_parent.id) if val.root_parent.is_a?(Forum)
-      val
     end
 
     def self.extended(base)
