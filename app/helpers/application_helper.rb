3788c7113fe4faa2946a1cc92142e94c6c3f59e6
Fletcher91 <thomvankalkeren@gmail.com>
2015-09-18 11:22:58 +0200

    [IMP] Open links in new tab/window

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -165,7 +165,7 @@ module ApplicationHelper
   # TODO: Something something instance variable with Redcarpet
   def markdown_to_html(markdown)
     Redcarpet::Markdown.new(
-        Redcarpet::Render::HTML.new(filter_html: false, escape_html: true),
+        Argu::Render::HTML.new(filter_html: false, escape_html: true),
         {tables: false, fenced_code_blocks: false, no_styles: true, escape_html: true, autolink: true, lax_spacing: true}
     ).render(markdown).html_safe
   end
