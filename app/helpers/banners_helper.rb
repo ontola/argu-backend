7ab072bf4e31d82b9131d34ec3f492c8a4799e62
Fletcher91 <thomvankalkeren@gmail.com>
2015-10-17 16:38:45 +0200

    [EOD]

--- app/helpers/banners_helper.rb
+++ app/helpers/banners_helper.rb
@@ -1,5 +1,9 @@
 module BannersHelper
 
+  def options_for_announcement_audiences
+    Announcement.audiences.keys.map { |n| [I18n.t("banners.audiences.#{n}"), n] }
+  end
+
   def options_for_banner_audiences
     Banner.audiences.keys.map { |n| [I18n.t("banners.audiences.#{n}"), n] }
   end
---
+++ test/controllers/banner_dismissals_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class BannerDismissalsControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
+  let!(:holland) { FactoryGirl.create(:populated_forum, name: 'holland') }
+
+end
