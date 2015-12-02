93d334b05c91ed26a2bd78868e1a8f4437da0d91
Fletcher91 <thom@argu.co>
2015-12-02 15:21:33 +0100

    [FIX] Let question creator follow the thing [FIX] commented out empty test

--- test/unit/stored_procedures/hello_world_test.rb
+++ test/unit/stored_procedures/hello_world_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 module StoredProcedureTests
 
   class HelloWorldTest < ActiveSupport::TestCase
-    def setup
-      @procedure = Squirm.procedure "hello_world"
-    end
-
-    test "hello_world should emit a greeting" do
-      assert_equal "hello world!", @procedure.call
-    end
+    # def setup
+    #   @procedure = Squirm.procedure "hello_world"
+    # end
+    #
+    # test "hello_world should emit a greeting" do
+    #   assert_equal "hello world!", @procedure.call
+    # end
   end
 
 end
