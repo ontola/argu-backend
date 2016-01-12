04a6e7964d49cf6d48c258dd0e1a1103f8c54fde
Joep Meindertsma <joepmeindertsma@gmail.com>
2016-01-12 13:54:21 +0100

    [IMP] Markdown interprets enters as newlines and converts to <br>

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,7 +1,7 @@
 module MarkdownHelper
   def markdown_to_html(markdown)
     Redcarpet::Markdown.new(
-        Argu::Render::HTML.new(filter_html: false, escape_html: true),
+        Argu::Render::HTML.new(filter_html: false, escape_html: true, hard_wrap: true),
         {tables: false, fenced_code_blocks: false, no_styles: true, escape_html: true, autolink: true, lax_spacing: true}
     ).render(markdown).html_safe
   end
