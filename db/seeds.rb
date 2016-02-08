35017bafb8c273cb83500506bfa0438e67d54ab2
Joep Meindertsma <joepmeindertsma@gmail.com>
2016-02-08 19:06:48 +0100

    [IMP] Typography colors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,17 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-ua = User.create(email: 'staff@argu.co', shortname_attributes: {shortname: 'staff_account'}, password: 'arguargu', password_confirmation:'arguargu')
-ub = User.create(email: 'user@argu.co', shortname_attributes: {shortname: 'user_account'}, password: 'arguargu', password_confirmation:'arguargu')
+ua = User.new(email: 'staff@argu.co', shortname_attributes: {shortname: 'staff_account'}, password: 'arguargu', password_confirmation:'arguargu')
+ub = User.new(email: 'user@argu.co', shortname_attributes: {shortname: 'user_account'}, password: 'arguargu', password_confirmation:'arguargu')
 
 ua.build_profile name: 'Staff Account'
 ub.build_profile name: 'User Account'
 
 ua.profile.add_role :staff
 ub.profile.add_role :user
+
+ua.save!
+ub.save!
 
 Setting.set('user_cap', -1)
 Setting.set('quotes', 'Argumenten moet men wegen, niet tellen.')
