224e8e945f5a77daee3e82a275ab2dbcaab16cb8
Thom Van Kalkeren <thomvankalkeren@gmail.com>
2016-02-14 12:49:11 +0000

    unicorn.rb edited online with Bitbucket

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,1 +1,12 @@
+app_dir = "/usr/src/app"
+ 
+working_directory app_dir
+ 
+pid "#{app_dir}/tmp/pids/unicorn.pid"
+ 
+stderr_path "#{app_dir}/log/unicorn.stderr.log"
+stdout_path "#{app_dir}/log/unicorn.stdout.log"
+ 
+worker_processes 2
+
 listen 3000, :tcp_nopush => false
