8c05f82335c72199033c704a1c92cae339a29f73
Fletcher91 <thom@argu.co>
2016-02-18 17:05:08 +0100

    [FIX] Ported Banner and Announcement to ActivePublishable (needed migration) [IMP] Added unpublished scope to ActivePublishable [IMP] Ported CreateAnnouncement and CreateBanner to implement CreateService [TEST] Manager creates a banner

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -7,4 +7,11 @@ module ProjectsHelper
       phase.end_date && "ends on #{phase.end_date.strftime('%A, %B %d %Y')}_"
     ].compact.join(' and ') || '_has no date set_'
   end
+
+  def start_end_title_short_string(phase)
+    [
+      phase.start_date && "#{l(phase.start_date, format: :date)}",
+      phase.end_date && "#{l(phase.end_date, format: :date)}"
+    ].compact.join(' - ')
+  end
 end
