a1debea2925211744f472e90ea3934776c186080
Fletcher91 <thom@argu.co>
2016-02-23 15:00:27 +0100

    [IMP] Redis url setting in sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 Sidekiq.logger.formatter = Sidekiq::Logging::Json::Logger.new if Rails.env.development?
 
 Sidekiq.configure_server do |config|
-  config.redis = {url: ENV['REDIS_URL']}
+  config.redis = {url: ENV['REDIS_URL'].presence || "redis://#{ENV['REDIS_ADDRESS'] || 'localhost'}:#{ENV['REDIS_PORT'] || 6379}/12"}
 end
