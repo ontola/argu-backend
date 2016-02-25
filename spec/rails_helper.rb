3d1dfe266b215a29658bf725b37993a593f0f2ff
Fletcher91 <thom@argu.co>
2016-02-25 10:02:04 +0100

    [FEATURE] Basic testingbot support

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,27 +10,25 @@ require 'capybara/rails'
 require 'database_cleaner'
 require 'sidekiq/testing'
 require 'capybara/poltergeist'
-
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-#
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-#
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+require 'testingbot'
+require 'testingbot/capybara'
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
 Dir[File.dirname(__FILE__) + '/support/**/*.rb'].each {|f| require f }
+
+TestingBot::config do |config|
+  config[:desired_capabilities] = {
+    browserName: ENV['BROWSER_NAME'] || 'internet explorer',
+    version: ENV['BROWSER_VERSION'] || '11',
+    platform: ENV['BROWSER_PLATFORM'] || 'WIN8',
+    api_key: ENV['TESTINGBOT_KEY'],
+    api_secret: ENV['TESTINGBOT_SECRET']
+  }
+  config.require_tunnel # uncomment if you want to use our Tunnel
+end
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
@@ -92,13 +90,15 @@ RSpec.configure do |config|
                               :webkit
                             when 'ie'
                               :internet_explorer
+                            when 'testingbot'
+                              :testingbot
                             when 'safari'
                               :selenium_safari
                             else
                               ENV['CI'].present? ? :selenium : :selenium
                             end
   #Capybara.default_max_wait_time = 5
-  Capybara.default_max_wait_time = 50
+  Capybara.default_max_wait_time = 10
 
   Capybara::Webkit.configure do |config|
     config.allow_url 'http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,700'
