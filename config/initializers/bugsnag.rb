7242c0318189c67f31ea369f2fc596589871fadc
Fletcher91 <thom@argu.co>
2016-02-26 11:35:23 +0100

    [IMP] Send versions along with Bugsnag and GGA

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,4 +1,5 @@
 Bugsnag.configure do |config|
   config.api_key = 'b65723480764039f0c48b01009935be9'
-  config.notify_release_stages = ['production', 'staging']
+  config.notify_release_stages = %w(production staging)
+  config.app_version = "v#{::VERSION}/#{::BUILD}"
 end
