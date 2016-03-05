4c27a30aa79f193a9264c3d20188ca7bcf15429e
Fletcher91 <thom@argu.co>
2016-03-05 18:17:47 +0100

    [IMP] Wrapped the discussions button and banner in `can_start_discussion?`

---
+++ app/helpers/discussions_helper.rb
@@ -0,0 +1,12 @@
+module DiscussionsHelper
+
+  # Checks if the user is able to start a top-level discussion in the current context/tenant
+  # @return [Boolean] Whether the user can new? any discussion object
+  def can_start_discussion?
+    [
+      [Question, :new?],
+      [Motion, :new_without_question?],
+      [Project, :new?]
+    ].any? { |model, method| policy_with_tenant!(@forum || authenticated_context, model).public_send(method) }
+  end
+end
