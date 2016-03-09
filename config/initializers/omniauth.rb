0378f2d9f5a77690360721826e059d84db6c6bac
Arthur Dingemans <arthur@argu.co>
2016-03-09 11:31:17 +0100

    [FIX] Request large profile images from Facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
   provider :facebook, Rails.application.secrets.facebook_key, Rails.application.secrets.facebook_secret,
-           scope: 'email', secure_image_url: true, :client_options => { :ssl => { :ca_file => "#{Rails.root}/config/ca-bundle.crt" } }
+           scope: 'email', secure_image_url: true, image_size: 'large', :client_options => { :ssl => { :ca_file => "#{Rails.root}/config/ca-bundle.crt" } }
 
   provider :twitter, Rails.application.secrets.twitter_key, Rails.application.secrets.twitter_secret,
            x_auth_access_type: 'write'
