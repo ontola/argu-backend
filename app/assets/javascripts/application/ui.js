0c43fc66b285d4def5f9a551f9ca8d2dd5698465
Fletcher91 <thom@argu.co>
2016-03-11 18:04:51 +0100

    [FIX] Added scroll event as a param causing the default action to be prevented

--- app/assets/javascripts/application/ui.js
+++ app/assets/javascripts/application/ui.js
@@ -78,7 +78,7 @@ const ui = {
         }
     },
 
-    handleClickSmoothly: function () {
+    handleClickSmoothly: function (event) {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
