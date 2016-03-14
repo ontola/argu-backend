a11a7b866f9f528a63249a663c64e01397d60dac
Fletcher91 <thom@argu.co>
2016-03-14 12:01:01 +0100

    [IMP] Let Banner use ActivePublishable [IMP] Show ended banners in their own segment

--- app/models/banners/banner.rb
+++ app/models/banners/banner.rb
@@ -6,15 +6,6 @@ class Banner < ActiveRecord::Base
 
   enum audience: { guests: 0, users: 1, members: 2, everyone: 3 }
 
-  scope :published, -> do
-    where('published_at <= ?', DateTime.current)
-      .where('ends_at IS NULL OR ends_at > ?', DateTime.current)
-  end
-  scope :unpublished, -> do
-    where('published_at IS NULL OR published_at > ?', DateTime.current)
-      .where('ends_at IS NULL OR ends_at <= ?', DateTime.current)
-  end
-
   validates :forum, :audience, presence: true
   #validates :sample_size, min: 1, max: 100, default: 100
 
