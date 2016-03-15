6923a74818a36cc1d42cad92d68777df20ed2ccb
Fletcher91 <thom@argu.co>
2016-03-15 19:34:30 +0100

    [FIX] Added flag image extensions for the assets pipeline

--- app/helpers/language_helper.rb
+++ app/helpers/language_helper.rb
@@ -2,14 +2,14 @@ module LanguageHelper
   def language_dropdown_items
     {
       title: I18n.locale.upcase,
-      image: { url: path_to_image("flags/#{I18n.locale}"), title: I18n.t(:language, locale: I18n.locale)},
+      image: { url: path_to_image("flags/#{I18n.locale}.svg"), title: I18n.t(:language, locale: I18n.locale)},
       sections: [
           {
               items: I18n.available_locales.collect {|language_code|
                     link_item(
                         I18n.t(:language, locale: language_code),
                         language_users_path(language_code),
-                        image: path_to_image("flags/#{language_code}"),
+                        image: path_to_image("flags/#{language_code}.svg"),
                         data: { method: :put }
                     )
                   }
