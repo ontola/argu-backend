b61876f35e8392dc8f4e151d961b18ebdd024c87
Arthur Dingemans <arthur@argu.co>
2016-03-30 13:48:34 +0200

    [IMP] Only send notifications for create activities

--- app/listeners/notification_listener.rb
+++ app/listeners/notification_listener.rb
@@ -1,18 +1,22 @@
 class NotificationListener
   def create_activity_successful(activity)
-    create_notifications_for(activity, activity.recipient.followers)
+    create_notifications_for(activity, collect_followers_for(activity)) if activity.action == 'create'
   end
 
   private
 
   def create_notifications_for(activity, followers)
-    notifications = collect_followers_for(activity, followers)
-    Notification.create!([notifications])
+    Notification.create!(prepare_followers(activity, followers))
   end
 
-  def collect_followers_for(activity, followers)
-    followers
+  def collect_followers_for(activity)
+    activity.recipient.followers
         .reject { |f| f.profile == activity.owner }
+  end
+
+  # @return [Array<Hash{Symbol => User, Symbol => Activity}>] List of attributes for {Notification} creation
+  def prepare_followers(activity, followers)
+    followers
         .map { |f| {user: f, activity: activity} }
   end
 end
