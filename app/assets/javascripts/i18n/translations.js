514abcd8f3c6b17c508dd5ad2ec0b5590077935b
Fletcher91 <thom@argu.co>
2016-03-31 12:51:02 +0200

    [IMP] Removed auto-generated translations.js from the tree

--- app/assets/javascripts/i18n/translations.js
+++
@@ -1,3 +0,0 @@
-I18n.translations || (I18n.translations = {});
-I18n.translations["nl"] = I18n.extend((I18n.translations["nl"] || {}), {});
-I18n.translations["en"] = I18n.extend((I18n.translations["en"] || {}), {});
