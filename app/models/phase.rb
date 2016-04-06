5f873e033663b6e55f53c7dd82676bcd7dec4793
Arthur Dingemans <arthur@argu.co>
2016-04-06 14:58:03 +0200

    [IMP] Validate end_date after start_date for phases
    [IMP] Update start_date of project, start_date of next_phase or end_date of project when editing dates of phases
    [IMP] Add methods to find previous and next phase, based on id

--- app/models/phase.rb
+++ app/models/phase.rb
@@ -10,6 +10,7 @@ class Phase < ActiveRecord::Base
   validates :forum, presence: true
   validates :project, presence: true
   validates :creator, presence: true
+  validate :end_date_after_start_date
 
   # For Rails 5 attributes
   # attribute :name, :string
@@ -18,6 +19,8 @@ class Phase < ActiveRecord::Base
   # attribute :start_date, :datetime
   # attribute :end_date, :datetime
   alias_attribute :display_name, :name
+
+  before_save :update_date_of_project_or_next_phase
 
   parentable :project
   counter_culture :project
@@ -35,5 +38,24 @@ class Phase < ActiveRecord::Base
           .where('published_at > ?', start_date)
           .order(published_at: :asc)
     end
+  end
+
+  def end_date_after_start_date
+    if start_date.present? && end_date.present? && end_date < start_date
+      errors.add(:end_date, "can't be before start date")
+    end
+  end
+
+  def next_phase
+    @next_phase ||= project.phases.where('id > ?', self.id).try(:first)
+  end
+
+  def previous_phase
+    @previous_phase ||= project.phases.where('id < ?', self.id).try(:last)
+  end
+
+  def update_date_of_project_or_next_phase
+    next_phase.present? ? next_phase.update(start_date: end_date) : project.update(end_date: end_date) if end_date_changed?
+    project.update(start_date: start_date) if start_date_changed? && !previous_phase.present?
   end
 end
