7b5d0303f3e566e9921ab5bb765820b71c837cba
Arthur Dingemans <arthur@argu.co>
2016-04-06 14:46:02 +0200

    [IMP] Set start_date of first phase to start_date of project
    [IMP] Order phases on id
    [IMP] Add method to get current_phase for project
    [IMP] Don't ask for dates when adding phases

--- app/models/project.rb
+++ app/models/project.rb
@@ -19,7 +19,7 @@ class Project < ActiveRecord::Base
   belongs_to :publisher, class_name: 'User'
 
   has_many   :motions, inverse_of: :project
-  has_many   :phases, inverse_of: :project
+  has_many   :phases, -> {order(:id)}, inverse_of: :project
   has_many   :stepups, as: :record, dependent: :destroy
   has_many   :questions, inverse_of: :project
   has_many   :activities, as: :trackable
@@ -29,12 +29,25 @@ class Project < ActiveRecord::Base
 
   validates :forum, :creator, presence: true
 
+  before_save :update_start_date_of_first_phase
   counter_culture :forum
   acts_as_followable
   parentable :forum
   mount_uploader :cover_photo, CoverUploader
 
+  def current_phase
+    phases.where('start_date < ?', Time.now).last
+  end
+
+  def in_last_phase?
+    phases.where('end_date IS NULL').count == 1
+  end
+
   def latest_blog_post
     blog_posts.order(published_at: :desc).first
+  end
+
+  def update_start_date_of_first_phase
+    phases.first.start_date = start_date if phases.present? && start_date_changed?
   end
 end
