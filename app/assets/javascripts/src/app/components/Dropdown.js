89999cdc6cc6a5b42e0c5b5b7b42b88a8239e9a1
Fletcher91 <thom@argu.co>
2016-04-07 11:25:53 +0200

    [GRAMMAR] Reverted HyperDropdown render() return statement to correct indentation

--- app/assets/javascripts/src/app/components/Dropdown.js
+++ app/assets/javascripts/src/app/components/Dropdown.js
@@ -61,16 +61,15 @@ export const HyperDropdown = React.createClass({
                                                {...this.props}
                                                key='required' />;
 
-        return (
-        <div tabIndex="1"
-        className={dropdownClass}
-        onMouseEnter={this.onMouseEnter}
-        onMouseLeave={this.onMouseLeave} >
-        {trigger}
-        <div className="reference-elem" style={{visibility: 'hidden', overflow: 'hidden', 'pointerEvents': 'none', position: 'absolute'}}>{dropdownContent}</div>
-        <ReactTransitionGroup transitionName="dropdown" transitionAppear={true} component="div">
-            {openState && dropdownContent}
-        </ReactTransitionGroup>
+        return (<div tabIndex="1"
+                     className={dropdownClass}
+                     onMouseEnter={this.onMouseEnter}
+                     onMouseLeave={this.onMouseLeave}>
+            {trigger}
+            <div className="reference-elem" style={{visibility: 'hidden', overflow: 'hidden', 'pointerEvents': 'none', position: 'absolute'}}>{dropdownContent}</div>
+            <ReactTransitionGroup transitionName="dropdown" transitionAppear={true} component="div">
+                {openState && dropdownContent}
+            </ReactTransitionGroup>
         </div>);
     }
 });
