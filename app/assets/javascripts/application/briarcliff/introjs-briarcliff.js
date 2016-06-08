1e1bd2d526b1f35ee89a5b6cf0e00922d2793ad8
Fletcher91 <thom@argu.co>
2016-06-08 14:35:53 +0200

    [FIX] Intro.js didn't work properly for some reason

--- app/assets/javascripts/application/briarcliff/introjs-briarcliff.js
+++ app/assets/javascripts/application/briarcliff/introjs-briarcliff.js
@@ -1,11 +1,7 @@
 import { introJs } from 'intro.js';
 import I18n from 'i18n-js';
 
-if(typeof I18n !== 'undefined') {
-    I18n.locale = document.head.querySelector("[name=language]").content;
-}
-
-let introJsOptions = {
+const introJsOptions = {
     'skipLabel': I18n.t('intro.skip'),
     'nextLabel': I18n.t('intro.next'),
     'prevLabel': I18n.t('intro.previous'),
@@ -18,38 +14,43 @@ let introJsOptions = {
     'tooltipPosition': 'bottom-middle-aligned'
 };
 
-// For some weird reason, IntroJS does not listen to default tooltipPosition, so I added the position manually to all steps.
-let introJsMotionTour = introJs().setOptions(introJsOptions).setOptions({
-    steps:[
-        {
-            element: document.querySelector('.motion-body'),
-            intro: I18n.t('intro.motion.posted.body'),
-            position: 'bottom-middle-aligned'
-        },
-        {
-            element: document.querySelector('.motion-votes'),
-            intro: I18n.t('intro.motion.posted.vote'),
-            position: 'bottom'
-        },
-        {
-            element: document.querySelector('.argument-columns'),
-            intro: I18n.t('intro.motion.posted.post_argument'),
-            position: 'bottom-middle-aligned'
-        },
-        {
-            element: document.querySelector('.share-menu'),
-            intro: I18n.t('intro.motion.posted.share'),
-            position: 'bottom-right-aligned'
-        }
-    ]
-});
-
-//start introJs after posting an idea
-if (window.location.search.indexOf('start_motion_tour=true') > -1) {
-    introJsMotionTour.start();
-}
-
 $(function() {
+    if(typeof I18n !== 'undefined') {
+        I18n.locale = document.head.querySelector("[name=language]").content;
+    }
+
+    //start introJs after posting an idea
+    if (window.location.search.indexOf('start_motion_tour=true') > -1) {
+        // For some weird reason, IntroJS does not listen to default tooltipPosition, so I added the position manually to all steps.
+        introJs()
+            .setOptions(introJsOptions)
+            .setOptions({
+                steps: [
+                    {
+                        element: document.querySelector('.motion-body'),
+                        intro: I18n.t('intro.motion.posted.body'),
+                        position: 'bottom-middle-aligned'
+                    },
+                    {
+                        element: document.querySelector('.motion-votes'),
+                        intro: I18n.t('intro.motion.posted.vote'),
+                        position: 'bottom'
+                    },
+                    {
+                        element: document.querySelector('.argument-columns'),
+                        intro: I18n.t('intro.motion.posted.post_argument'),
+                        position: 'bottom-middle-aligned'
+                    },
+                    {
+                        element: document.querySelector('.share-menu'),
+                        intro: I18n.t('intro.motion.posted.share'),
+                        position: 'bottom-right-aligned'
+                    }
+                ]
+            })
+            .start();
+    }
+
     $(document)
         // read all the introJs content on the page. May be deprecated.
         .on("click", '.intro-trigger', function () {
