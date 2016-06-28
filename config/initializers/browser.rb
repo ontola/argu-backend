34320d2b7c30d5c71b981dba9a54faa204d0d81a
Arthur Dingemans <arthur@argu.co>
2016-06-28 14:53:15 +0200

    [FIX] Update modern browser definitions [FIX] Include polyfill in iOS

--- config/initializers/browser.rb
+++ config/initializers/browser.rb
@@ -3,13 +3,13 @@
 #
 Browser.modern_rules.clear
 Browser.modern_rules << lambda do |b|
-  (b.mobile? || b.tablet?) && (b.chrome? && b.version.to_i > 41 || b.safari? && b.version.to_i > 7)
+  (b.mobile? || b.tablet?) && (b.chrome? && b.version.to_i >= 51 || b.safari? && b.version.to_i >= 9.1)
 end
 # Browser.modern_rules << -> b { !b.ie? } # IE11 doesn't support Promise/A+
 Browser.modern_rules << lambda do |b|
   !(b.mobile? && b.tablet?) && \
-    b.chrome?  && b.version.to_i > 41 || \
-    b.firefox? && b.version.to_i > 26 || \
-    b.opera?   && b.version.to_i > 26 || \
-    b.safari?  && b.full_version.to_f > 7.1
+    b.chrome?  && b.version.to_i >= 51 || \
+    b.firefox? && b.version.to_i >= 47 || \
+    b.opera?   && b.version.to_i >= 38 || \
+    b.safari?  && b.full_version.to_f >= 9.1
 end
