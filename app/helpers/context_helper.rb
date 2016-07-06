92291da7001d96742ea2dea556a0729062f93b9d
Arthur Dingemans <arthur@argu.co>
2016-07-06 13:19:04 +0200

    [IMP] Replace contextual_link_to with link_to

--- app/helpers/context_helper.rb
+++ app/helpers/context_helper.rb
@@ -17,18 +17,6 @@ module ContextHelper
     end
   end
 
-  # Generates a link with the current context kept
-  def contextual_link_to(name = nil, options = nil, html_options = nil, &block)
-    # @TODO: Fix the context parsing system (https://trello.com/c/KTJZJ4Bp/312-context-system-refactor)
-    # @note: Disabled until above is done
-    # if block_given?
-    #   name = merge_query_parameter(name, current_context.to_query) if name.class == String
-    # else
-    #   options = merge_query_parameter(options, current_context.to_query) if options.class == String
-    # end
-    link_to name, options, html_options, &block
-  end
-
   def forum_from_scope_or_model(current_scope, model)
     if model.respond_to? :uses_alternative_names
       model
