38ec56d8c2b2c271706a9f60fca64464bd9ec56c
Fletcher91 <thom@argu.co>
2016-09-16 16:54:02 +0200

    [FIX] Return mail config in production environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "argu_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = {host: 'https://argu.co'}
+  Rails.application.routes.default_url_options[:host] = 'https://argu.co'
+  config.roadie.url_options = {host: 'argu.co', scheme: 'https'}
+  config.action_mailer.asset_host = nil
+
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+    api_key: Rails.application.secrets.mailgun_api_token,
+    domain: Rails.application.secrets.mailgun_domain
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
