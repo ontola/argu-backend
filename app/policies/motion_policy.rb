ea5085b37501f5d0cbfa2772dcedec7f2e4ebda4
Arthur Dingemans <arthur@argu.co>
2016-10-04 16:28:07 +0100

    [FIX] Allow managers to create in Question 488

--- app/policies/motion_policy.rb
+++ app/policies/motion_policy.rb
@@ -34,7 +34,7 @@ class MotionPolicy < RestrictivePolicy
 
   def permitted_attributes
     attributes = super
-    attributes.concat %i(title content votes tag_list question_id) if create?
+    attributes.concat %i(title content votes tag_list question_id) if create? || update?
     attributes.concat %i(invert_arguments tag_id forum_id f_convert) if staff?
     attributes.append :id if record.is_a?(Motion) && edit?
     attributes.append(question_answers_attributes: %i(id question_id motion_id)) if create?
@@ -51,7 +51,7 @@ class MotionPolicy < RestrictivePolicy
   end
 
   def create?
-    return false if record.question&.id == 488
+    return false if record.question&.id == 488 && !is_manager?
     return false if record.question.present? && record.question.expired?
     rule is_member?, is_manager?, is_owner?, super
   end
@@ -77,7 +77,7 @@ class MotionPolicy < RestrictivePolicy
   end
 
   def new?
-    return false if record.question&.id == 488
+    return false if record.question&.id == 488 && !is_manager?
     return false if record.question.present? && record.question.expired?
     rule is_open?, is_member?, is_manager?, staff?
   end
