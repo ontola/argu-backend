bcc8a0c4a98d2469091d36b573be693d61010a20
Arthur Dingemans <arthur@argu.co>
2016-10-31 13:58:24 +0100

    [CLEAN] Remove all unnecessary untrash services

--- app/services/cruudt_services/base/untrash_service.rb
+++ app/services/cruudt_services/base/untrash_service.rb
@@ -2,10 +2,14 @@
 # Superclass for all the services that trash records
 # @author Arthur <arthur@argu.co>
 class UntrashService < ApplicationService
+  include Wisper::Publisher
+
   # @note Call super when overriding.
   def initialize(resource, attributes: {}, options: {})
+    @resource = resource
     super
   end
+  attr_reader :resource
 
   private
 
