aa9cf222a8dc771cecb23c480ea048b952d7f5b4
Arthur Dingemans <arthur@argu.co>
2016-11-16 11:06:46 +0100

    [FIX] Oauth application management. authenticate_user! was unable to authenticate the user.

--- app/controllers/oauth/applications_controller.rb
+++ app/controllers/oauth/applications_controller.rb
@@ -1,21 +1,29 @@
 # frozen_string_literal: true
 # app/controllers/oauth/applications_controller.rb
 class Oauth::ApplicationsController < Doorkeeper::ApplicationsController
-  before_action :authenticate_user!
-
   def index
-    @applications = current_user.oauth_applications
+    @applications = current_resource_owner.oauth_applications
   end
 
   # only needed if each application must have some owner
   def create
     @application = Doorkeeper::Application.new(application_params)
-    @application.owner = current_user if Doorkeeper.configuration.confirm_application_owner?
+    @application.owner = current_resource_owner if Doorkeeper.configuration.confirm_application_owner?
     if @application.save
       flash[:notice] = I18n.t(:notice, scope: [:doorkeeper, :flash, :applications, :create])
       respond_with(:oauth, @application, location: oauth_application_url(@application))
     else
       render :new
     end
+  end
+
+  private
+
+  def authenticate_admin!
+    current_resource_owner&.profile&.has_role?(:staff)
+  end
+
+  def current_resource_owner
+    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
   end
 end
