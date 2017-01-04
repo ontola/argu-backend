8cebc5172057a81838d5fa480eb24978691fc118
Arthur Dingemans <arthur@argu.co>
2017-01-04 11:12:29 +0100

    [IMP] Use option instead of for in vote serialiser

--- app/serializers/vote_serializer.rb
+++ app/serializers/vote_serializer.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 class VoteSerializer < BaseEdgeSerializer
-  attributes :for
+  attributes :option
+
+  def option
+    case object.for
+    when 'pro'
+      'http://schema.org/yes'
+    when 'con'
+      'http://schema.org/no'
+    else
+      'http://schema.org/neutral'
+    end
+  end
 end
