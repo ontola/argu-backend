25c1947931c017db092109fc3731287bf0711992
Arthur Dingemans <arthur@argu.co>
2017-01-04 10:11:17 +0100

    [IMP] Add upvoted arguments to vote serializer

--- app/serializers/vote_serializer.rb
+++ app/serializers/vote_serializer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class VoteSerializer < BaseEdgeSerializer
-  attributes :option
+  attributes :option, :upvoted_arguments
 
   def option
     case object.for
@@ -11,5 +11,12 @@ class VoteSerializer < BaseEdgeSerializer
     else
       'http://schema.org/neutral'
     end
+  end
+
+  def upvoted_arguments
+    Vote
+      .joins(edge: :parent)
+      .where(voter: object.voter, parents_edges: {parent_id: object.edge.parent_id})
+      .map { |vote| vote.parent_model.context_id }
   end
 end
