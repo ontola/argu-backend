53db09c041648f9cf2af3db8bb812ed46d80d78c
Arthur Dingemans <arthur@argu.co>
2017-01-05 14:27:30 +0100

    [IMP] Overwrite type of a ListItem with the given resource_type in ListItemSerializer
    See https://github.com/rails-api/active_model_serializers/pull/1777

--- app/serializers/list_item_serializer.rb
+++ app/serializers/list_item_serializer.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 class ListItemSerializer < BaseSerializer
+  def self.type(type = nil, &block)
+    self._type = block || type
+  end
+  type(&:resource_type)
+
   def id
     ld_id
   end
