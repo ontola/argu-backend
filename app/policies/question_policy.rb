71739758e1f2340e38f5b116afa9a6ee2762b0fb
Arthur Dingemans <arthur@argu.co>
2017-01-10 14:50:34 +0100

    [ARGU-678] Remove active_for :expires_at [IMP] Add :expires_at to permitted_attributes only for manager/staff

--- app/policies/question_policy.rb
+++ app/policies/question_policy.rb
@@ -51,10 +51,6 @@ class QuestionPolicy < EdgeTreePolicy
       super
   end
 
-  def set_expire_as?
-    rule staff?
-  end
-
   def show?
     return show_unpublished? if has_unpublished_ancestors?
     rule has_access_token?, is_member?, is_manager?, is_owner?, super
