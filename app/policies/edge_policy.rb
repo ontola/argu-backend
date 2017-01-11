00978fbcabbf41b433d2bcb660528ea97e15b17b
Arthur Dingemans <arthur@argu.co>
2017-01-11 09:26:51 +0100

    [IMP] Make motions expireable

--- app/policies/edge_policy.rb
+++ app/policies/edge_policy.rb
@@ -5,7 +5,7 @@ class EdgePolicy < EdgeTreePolicy
   def permitted_attributes
     attributes = super
     attributes.concat %i(id is_trashed trashed_at) if record.owner.is_trashable?
-    attributes.concat %i(id expires_at) if %W(Question).include?(record.owner_type)
+    attributes.concat %i(id expires_at) if %w(Motion Question).include?(record.owner_type)
     attributes.append(argu_publication_attributes: %i(id publish_type published_at)) if record.owner.is_publishable?
     attributes
   end
