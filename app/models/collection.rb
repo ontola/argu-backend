181972cb7067c43111658a13f75d793c741c53f7
Arthur Dingemans <arthur@argu.co>
2017-01-16 17:07:14 +0100

    [REF] Better collections, with pagination and filters

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,13 +1,97 @@
 # frozen_string_literal: true
 class Collection
   include ActiveModel::Model, ActiveModel::Serialization, PragmaticContext::Contextualizable,
-          Ldable
+          Ldable, Pundit
+  include ActionDispatch::Routing
+  include Rails.application.routes.url_helpers
 
-  attr_accessor :association, :group_by, :member, :id, :parent, :potential_action, :title
+  attr_accessor  :association, :filter, :current_profile, :current_user, :page, :pagination,
+                 :parent, :potential_action, :title
+  attr_writer :association_class
 
-  contextualize_as_type 'hydra:Collection'
+  contextualize_as_type 'argu:Collection'
   contextualize_with_id(&:id)
-  contextualize :member, as: 'hydra:member'
   contextualize :title, as: 'schema:name'
-  contextualize :group_by, as: 'argu:groupBy'
+
+  def as_json(options = {})
+    super(options.merge(except: ['association_class']))
+  end
+
+  def id
+    opts = {}
+    opts[:page] = page if page.present?
+    opts[:filter] = filter if filter.present?
+    opts[:protocol] = :https
+    url_for([parent, association_class, opts])
+  end
+
+  def members
+    return if paginate? && page.nil?
+    if association == :votes
+      policy_scope(
+        parent
+          .send(association)
+          .includes(voter: :profileable, edge: :parent)
+          .where(filter_query)
+          .page(page)
+      )
+    else
+      policy_scope(
+        parent
+          .send(association)
+          .includes(creator: :profileable, edge: :parent)
+          .where(filter_query)
+          .page(page)
+      )
+    end
+  end
+
+  def views
+    if paginate?
+      return [Collection.new(page: 1, parent: parent, association: association, filter: filter, pagination: pagination)]
+    end
+    return if @views&.map(&:id)&.any? { |view_id| id.include?(view_id) }
+    @views
+  end
+
+  def views=(views)
+    @views = views.map do |view|
+      view.parent = parent
+      view.association = association
+      view.filter = (view.filter || {}).merge(filter) if filter.present?
+      view
+    end
+  end
+
+  def title
+    @title || I18n.t("#{association_class.name.tableize}.plural", default: association_class.name.tableize.humanize)
+  end
+
+  private
+
+  def association_class
+    @association_class ||= association.to_s.classify.constantize
+  end
+
+  def pundit_user
+    UserContext.new(
+      @current_user,
+      @current_profile,
+      nil
+    )
+  end
+
+  def filter_query
+    return if filter.nil?
+    filter.map do |key, value|
+      [
+        association_class.filter_options.fetch(key)[:key] || key,
+        association_class.filter_options.fetch(key)[:values].try(:[], value.to_sym) || value
+      ]
+    end.to_h
+  end
+
+  def paginate?
+    pagination.present? && page.nil? && filter.present?
+  end
 end
