fde488dcbcbc00c26c785ff634ce2b3283958af1
Arthur Dingemans <arthur@argu.co>
2017-02-24 13:07:53 +0100

    [IMP] Skip check if registered for list_items#index

--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class ListItemsController < AuthorizedController
   include NestedResourceHelper
+  skip_before_action :check_if_registered, only: :index
 
   def index
     skip_verify_policy_scoped(true)
