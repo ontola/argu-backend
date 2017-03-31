96c3de42922532225e373ecc1c919093e7c92ba8
Arthur Dingemans <arthur@argu.co>
2017-03-31 10:20:52 +0200

    [ARGU-439] Fix feed button translations

--- app/assets/javascripts/application/activity_feed.js
+++ app/assets/javascripts/application/activity_feed.js
@@ -1,4 +1,5 @@
 import Blazy from 'blazy';
+import I18n from 'i18n-js';
 
 let loading = false;
 let reachedEnd = false;
@@ -17,7 +18,7 @@ const activityFeed = {
     loadMore: function () {
         var _this   = $(this),
             feedDOM = $('.activity-feed .activities');
-        _this.text('activities.ui.loading');
+        _this.text(I18n.t('activities.ui.loading'));
         loading = true;
         $.ajax("/activities.html", {
             data: {
@@ -27,12 +28,12 @@ const activityFeed = {
             success: (d, s, xhr) => {
                 if (xhr.status == 200 || xhr.status == 304) {
                     feedDOM.append(d);
-                    _this.text('activities.ui.load_more');
+                    _this.text(I18n.t('activities.ui.load_more'));
                     var bLazy = new Blazy({
                         offset: 100 // Loads images 100px before they're visible
                     });
                 } else if (xhr.status == 204) {
-                    _this.text('activities.ui.no_more_artivities');
+                    _this.text(I18n.t('activities.ui.no_more_artivities'));
                     reachedEnd = true;
                     window.setTimeout(() => {
                         reachedEnd = false;
