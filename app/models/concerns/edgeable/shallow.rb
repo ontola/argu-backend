53a252b7434fbee379fcd763f5124928ddafd89e
Fletcher91 <thom@argu.co>
2017-04-08 16:29:29 +0200

    wip 10

--- app/models/concerns/edgeable/shallow.rb
+++ app/models/concerns/edgeable/shallow.rb
@@ -1,38 +1,40 @@
 # frozen_string_literal: true
 
 # Implements the basic edgeable interface without actually persisting any data.
-module Edgeable::Shallow
-  extend ActiveSupport::Concern
+module Edgeable
+  module Shallow
+    extend ActiveSupport::Concern
 
-  included do
-    attr_reader :edge
+    included do
+      attr_reader :edge
 
-    has_one :edge,
-            as: :owner,
-            inverse_of: :owner,
-            required: false
+      has_one :edge,
+              as: :owner,
+              inverse_of: :owner,
+              required: false
 
-    before_validation :remove_edge
+      before_validation :remove_edge
 
-    def root_object?
-      false
+      def root_object?
+        false
+      end
+
+      def respond_to?(method)
+        return false if method == :edge
+        super
+      end
+
+      private
+
+      def remove_edge
+        @edge = nil
+      end
     end
 
-    def respond_to?(method)
-      return false if method == :edge
-      super
-    end
-
-    private
-
-    def remove_edge
-      @edge = nil
-    end
-  end
-
-  module ClassMethods
-    def counter_cache_options
-      nil
+    module ClassMethods
+      def counter_cache_options
+        nil
+      end
     end
   end
 end
