8e3a137b12a27560af9b58050a510f89a027c8c1
Arthur Dingemans <arthur@argu.co>
2017-04-25 09:40:39 +0200

    [ARGU-914] Authorize CurrentActor in SPI::AuthorizeController

--- app/controllers/spi/authorize_controller.rb
+++ app/controllers/spi/authorize_controller.rb
@@ -2,9 +2,19 @@
 module SPI
   class AuthorizeController < SPI::SPIController
     def show
-      model = ApplicationRecord.descendants.detect { |m| m.to_s == params[:resource_type] }
-      authorize model.find(params[:resource_id]), "#{params[:authorize_action]}?"
+      authorize resource, "#{params[:authorize_action]}?"
       head 200
+    end
+
+    private
+
+    def resource
+      case params[:resource_type]
+      when 'CurrentActor'
+        CurrentActor.new(user: current_user, actor: Profile.find(params[:resource_id]))
+      else
+        ApplicationRecord.descendants.detect { |m| m.to_s == params[:resource_type] }.find(params[:resource_id])
+      end
     end
   end
 end
--- test/integration/spi/authorize_controller_test.rb
+++ test/integration/spi/authorize_controller_test.rb
@@ -21,6 +21,22 @@ module SPI
       assert_response 403
     end
 
+    test 'guest should not show page actor' do
+      get spi_authorize_path(
+        resource_type: 'CurrentActor', resource_id: freetown.page.profile.id, authorize_action: 'show'
+      )
+
+      assert_response 403
+    end
+
+    test 'guest should not show user actor' do
+      get spi_authorize_path(
+        resource_type: 'CurrentActor', resource_id: user.profile.id, authorize_action: 'show'
+      )
+
+      assert_response 403
+    end
+
     ####################################
     # As User
     ####################################
@@ -42,6 +58,24 @@ module SPI
       assert_response 403
     end
 
+    test 'user should not show page actor' do
+      sign_in user
+
+      get spi_authorize_path(
+        resource_type: 'CurrentActor', resource_id: freetown.page.profile.id, authorize_action: 'show'
+      )
+
+      assert_response 403
+    end
+
+    test 'user should show user actor' do
+      sign_in user
+
+      get spi_authorize_path(resource_type: 'CurrentActor', resource_id: user.profile.id, authorize_action: 'show')
+
+      assert_response 200
+    end
+
     ####################################
     # As Creator
     ####################################
@@ -61,6 +95,29 @@ module SPI
       get spi_authorize_path(resource_type: 'Motion', resource_id: motion.id, authorize_action: 'update')
 
       assert_response 200
+    end
+
+    ####################################
+    # As Manager
+    ####################################
+    let(:manager) { create_manager(freetown.page) }
+
+    test 'manager should show page actor' do
+      sign_in manager
+
+      get spi_authorize_path(
+        resource_type: 'CurrentActor', resource_id: freetown.page.profile.id, authorize_action: 'show'
+      )
+
+      assert_response 200
+    end
+
+    test 'manager should not show user actor' do
+      sign_in manager
+
+      get spi_authorize_path(resource_type: 'CurrentActor', resource_id: user.profile.id, authorize_action: 'show')
+
+      assert_response 403
     end
   end
 end
