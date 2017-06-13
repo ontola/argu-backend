20426d943e9b56ff897130cf3fed8e954624d64f
Arthur Dingemans <arthur@argu.co>
2017-06-13 14:35:19 +0200

    [ARGU-913] Improve error message when trying to persist a non-persistent record

--- app/models/concerns/no_persistence.rb
+++ app/models/concerns/no_persistence.rb
@@ -3,8 +3,8 @@ require 'argu/no_persistence_error'
 module NoPersistence
   extend ActiveSupport::Concern
   included do
-    def raise_on_persisting
-      raise Argu::NoPersistenceError
+    def raise_on_persisting(_opts = {})
+      raise Argu::NoPersistenceError.new("#{self.class.name} should not be persisted")
     end
     ActiveRecord::Persistence.instance_methods.each do |method|
       alias_method method, :raise_on_persisting unless method.to_s.include?('?')
