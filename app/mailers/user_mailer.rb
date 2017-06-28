cc6825135de5b49a2cc37d0b515a9fcc7c84e954
Arthur Dingemans <arthur@argu.co>
2017-06-28 13:02:52 +0200

    [ARGU-923] Remove the password_changed mailer. This mail is already sent by the ES

--- app/mailers/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-
-# frozen_string_literal: true
-class UserMailer < ApplicationMailer
-  def user_password_changed(user)
-    @user = user
-    mail to: @user.email,
-         subject: t('devise.registrations.password_confirmation.header')
-  end
-end
