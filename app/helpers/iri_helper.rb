2fdb734140ab3f221dcf948e2cb03146f7cafaa5
Arthur Dingemans <arthur@argu.co>
2017-06-30 17:33:03 +0200

    [ARGU-990] Fix IRIHelper#resource_from_iri when class does not inherit from ApplicationRecord
    For example /i/about

--- app/helpers/iri_helper.rb
+++ app/helpers/iri_helper.rb
@@ -27,6 +27,6 @@ module IRIHelper
   # @return [ApplicationRecord, nil] The resource corresponding to the iri, or nil if the IRI is not found
   def resource_from_iri(iri)
     id, type = id_and_type_from_iri(iri).values
-    ApplicationRecord.descendants.detect { |m| m.to_s == type.classify }.find(id) if id.present? || type.present?
+    ApplicationRecord.descendants.detect { |m| m.to_s == type.classify }&.find(id) if id.present? || type.present?
   end
 end
