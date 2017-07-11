9f50679dc8142244edc240bebe5b96cd3df13c2a
Arthur Dingemans <arthur@argu.co>
2017-07-11 09:21:49 +0200

    [ARGU-1017] resource_by_iri should handle short nameable

--- app/helpers/iri_helper.rb
+++ app/helpers/iri_helper.rb
@@ -27,6 +27,8 @@ module IRIHelper
   # @return [ApplicationRecord, nil] The resource corresponding to the iri, or nil if the IRI is not found
   def resource_from_iri(iri)
     id, type = id_and_type_from_iri(iri).values
-    ApplicationRecord.descendants.detect { |m| m.to_s == type.classify }&.find(id) if id.present? || type.present?
+    klass = ApplicationRecord.descendants.detect { |m| m.to_s == type.classify } if id.present? || type.present?
+    return if klass.nil?
+    klass.shortnameable? ? klass.find_via_shortname!(id) : klass.find(id)
   end
 end
---
+++ test/unit/iri_helper_test.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+require 'test_helper'
+
+class IriHelperTest < ActiveSupport::TestCase
+  include IRIHelper
+  define_freetown
+
+  test 'should find forum by its iri' do
+    resource_from_iri(argu_url('/freetown'))
+  end
+
+  test 'should find forum by its cannonical iri' do
+    resource_from_iri(argu_url("/f/#{freetown.id}"))
+  end
+
+  test 'should not find forum by non existing iri' do
+    assert_raises ActiveRecord::RecordNotFound do
+      resource_from_iri(argu_url('/non_existent'))
+    end
+  end
+end
