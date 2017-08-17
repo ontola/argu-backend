2e43325a5dc250f2dc893d849bd93d4b0e117e65
Arthur Dingemans <arthur@argu.co>
2017-08-17 15:09:10 +0200

    [ARGU-1098] Rename Placement#title to Placement#placement_type in migration file

--- db/migrate/20170815135921_create_forum_country_places.rb
+++ db/migrate/20170815135921_create_forum_country_places.rb
@@ -3,6 +3,6 @@ class CreateForumCountryPlaces < ActiveRecord::Migration[5.0]
     pre_count = Placement.count
     Forum.find_each { |forum| forum.send(:reset_country) }
     raise "Missing #{Placement.count - pre_count - Forum.count} placements" unless Placement.count - pre_count == Forum.count
-    raise "Missing #{Placement.where(title: 'country').count - Forum.count} placements" unless Placement.where(title: 'country').count == Forum.count
+    raise "Missing #{Placement.where(placement_type: 'country').count - Forum.count} placements" unless Placement.where(placement_type: 'country').count == Forum.count
   end
 end
