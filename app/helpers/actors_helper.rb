bfa304cb5876e08488af1dcca25d8e0ded712d5e
Arthur Dingemans <arthur@argu.co>
2017-08-31 16:55:13 +0200

    [ARGU-1123] Optimize Motion#edit

--- app/helpers/actors_helper.rb
+++ app/helpers/actors_helper.rb
@@ -3,12 +3,13 @@ require 'argu/not_authorized_error'
 
 module ActorsHelper
   def managed_profiles_list
-    current_user.managed_profiles.includes(:default_profile_photo, :profileable).map do |profile|
-      {
-        label: profile.display_name,
-        image: profile.default_profile_photo.url(:icon),
-        value: profile.profileable.context_id
-      }
-    end
+    @managed_profiles_list ||=
+      current_user.managed_profiles.includes(:default_profile_photo, :profileable).map do |profile|
+        {
+          label: profile.display_name,
+          image: profile.default_profile_photo.url(:icon),
+          value: profile.profileable.context_id
+        }
+      end
   end
 end
