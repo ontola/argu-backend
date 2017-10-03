7f11f75ecaa54712a233edb32e9a5df07c63d7d2
Arthur Dingemans <arthur@argu.co>
2017-10-03 12:10:43 +0200

    [ARGU-1131] Remove scopify from Role

--- app/models/role.rb
+++ app/models/role.rb
@@ -4,7 +4,5 @@ class Role < ApplicationRecord
   has_and_belongs_to_many :profiles, join_table: 'users_roles'
   belongs_to :resource, polymorphic: true
 
-  scopify
-
   validates :name, presence: true
 end
