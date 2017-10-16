9b5bad709f70dbd139ecc3ac562d217123d5f159
Arthur Dingemans <arthur@argu.co>
2017-10-16 10:01:57 +0200

    [ARGU-1224] Fix regex for email validation to enable -@ and --, while still disabling utf-8 characters like ë.
    The part before @ comes from the HTML5 http://emailregex.com/

--- app/javascript/components/tokens/InvitedSelection.js
+++ app/javascript/components/tokens/InvitedSelection.js
@@ -56,7 +56,7 @@ export const InvitedSelection = React.createClass({
     },
 
     invalidEmail (email) {
-        const re = /^\w+([\.-]?\w+)*(\+\w+)?@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const re = /^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return !re.test(email);
     },
 
