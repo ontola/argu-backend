25debf02a2525c33c2d9cd8f270f2246732077bf
Arthur Dingemans <arthur@argu.co>
2017-10-18 12:25:48 +0200

    [ARGU-1205] Remove edit-as selection, just show the current user

--- app/helpers/actors_helper.rb
+++ app/helpers/actors_helper.rb
@@ -6,11 +6,15 @@ module ActorsHelper
   def managed_profiles_list
     @managed_profiles_list ||=
       current_user.managed_profiles.includes(:default_profile_photo, :profileable).map do |profile|
-        {
-          label: profile.display_name,
-          image: profile.default_profile_photo.url(:icon),
-          value: profile.profileable.context_id
-        }
+        managed_profiles_list_item(profile)
       end
+  end
+
+  def managed_profiles_list_item(profile)
+    {
+      label: profile.display_name,
+      image: profile.default_profile_photo.url(:icon),
+      value: profile.profileable.context_id
+    }
   end
 end
