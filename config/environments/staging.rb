b3a70043cdfaa4996e7f075165f1169ed3a1cd2b
Fletcher91 <thom@argu.co>
2017-10-26 12:00:59 +0200

    [ARGU-355][ARGU-1251] Set cache headers in production and staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -25,10 +25,14 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.headers = {
+    'Cache-Control' => 'public, max-age=31536000',
+    'Expires' => 1.year.from_now.to_formatted_s(:rfc822)
+  }
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
