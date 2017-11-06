b9ca9a49e675cb3145e08da77804c483dfd70865
Arthur Dingemans <arthur@argu.co>
2017-11-06 14:10:08 +0100

    [ARGU-1270] Arguments don’t have a vote_collection

--- app/models/argument.rb
+++ app/models/argument.rb
@@ -9,6 +9,7 @@ class Argument < Edgeable::Content
   counter_cache arguments_pro: {pro: true}, arguments_con: {pro: false}
   filterable option: {key: :pro, values: {yes: true, no: false}}
   paginates_per 10
+  with_collection :votes, pagination: true
 
   validate :assert_tenant
 
