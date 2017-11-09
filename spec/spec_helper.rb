2fe5ba78313da270f8a0468f9a2941091feab8c5
Arthur Dingemans <arthur@argu.co>
2017-11-09 11:04:41 +0100

    [ARGU-1267] Set rspec retry count from env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -97,7 +97,7 @@ RSpec.configure do |config|
   config.verbose_retry = true
   config.display_try_failure_messages = true
   config.around :each do |ex|
-    ex.run_with_retry retry: 3
+    ex.run_with_retry retry: ENV['RSPEC_RETRY'] || 0
   end
 
   config.include(Wisper::RSpec::BroadcastMatcher)
