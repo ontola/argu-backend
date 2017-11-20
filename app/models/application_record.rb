8e63bf3365b23d8204dd4d519882c220291a8f7b
Fletcher91 <thom@argu.co>
2017-11-20 12:40:51 +0100

    [ARGU-1277][SM-21] Use ApplicationModel concern in ApplicationRecord.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,27 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationRecord < ActiveRecord::Base
+  include ApplicationModel
   include Iriable
 
   self.abstract_class = true
 
   %w[comment page forum question motion notification argument project blog_post group edge].each do |model|
     require_dependency model
-  end
-
-  def class_name
-    self.class.name.tableize
-  end
-
-  def self.class_name
-    name.tableize
-  end
-
-  def edited?
-    updated_at - 2.minutes > created_at
-  end
-
-  def identifier
-    "#{class_name}_#{id}"
   end
 end
