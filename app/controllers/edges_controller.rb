721f3e9ddeaa770fa5a3f4917de0a0d8fa5cf319
Arthur Dingemans <arthur@argu.co>
2018-02-07 10:13:57 +0100

    [ARGU-1370] Redirect edges when id is uuid

--- app/controllers/edges_controller.rb
+++ app/controllers/edges_controller.rb
@@ -7,6 +7,15 @@ class EdgesController < AuthorizedController
 
   private
 
+  def resource_by_id
+    @_resource_by_id ||=
+      if resource_id.match?(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/)
+        Edge.find_by(uuid: resource_id)
+      else
+        Edge.find_by(id: resource_id)
+      end
+  end
+
   def tree_root_id
     authenticated_resource.root_id
   end
