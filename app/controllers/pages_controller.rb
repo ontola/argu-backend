c3fc099012e0f69bedb15125bfcfa226ea97cd06
Arthur Dingemans <arthur@argu.co>
2018-03-15 14:58:43 +0100

    [ARGU-1419] Fix redirect from /o/:id to /o/:shortname

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -173,7 +173,7 @@ class PagesController < EdgeableController
 
   def show_respond_success_html(resource)
     if (/[a-zA-Z]/i =~ params[:id]).nil?
-      redirect_to resource.iri(only_path: true), status: 307
+      redirect_to resource.iri(only_path: true).to_s, status: 307
     else
       render 'show'
     end
