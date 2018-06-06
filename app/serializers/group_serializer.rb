beb89a43a3de88f87ad77ae6e8ab0af5d6542b23
Arthur Dingemans <arthur@argu.co>
2018-06-06 15:51:34 +0200

    [ARGU-1477] Add organization to GroupSerializer
    No predicate is needed, it is only required for the email service, which uses json_api

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -3,6 +3,10 @@
 class GroupSerializer < RecordSerializer
   include Parentable::Serializer
 
+  has_one :organization do
+    object.parent
+  end
+
   has_one :creator, predicate: NS::SCHEMA[:creator] do
     nil
   end
