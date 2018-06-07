a3e66a1b5ff7e8b142b0875539d9a15696b239cd
Fletcher91 <thom@argu.co>
2018-06-07 10:58:45 +0200

    [ARGU-1479] Move action-specific icons from the service module

--- app/models/actions/edgeable_collection_actions.rb
+++ app/models/actions/edgeable_collection_actions.rb
@@ -2,5 +2,13 @@
 
 module Actions
   class EdgeableCollectionActions < CollectionActions
+    include VotesHelper
+
+    private
+
+    def image
+      return 'fa-plus' unless filtered_resource? && resource.filter['option'].present?
+      "fa-#{icon_for_side(resource.filter['option'])}"
+    end
   end
 end
