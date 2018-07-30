5dbf50acf2c8aa98eb472ca6bd132a27b2710d0d
Arthur Dingemans <arthur@argu.co>
2018-07-30 15:58:46 +0200

    [ARGU-1483] No longer overwrite AR connection for spec tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -217,18 +217,3 @@ module FactoryBot
     end
   end
 end
-
-module ActiveRecord
-  class Base
-    mattr_accessor :shared_connection
-    @@shared_connection = nil
-
-    def self.connection
-      @@shared_connection || ConnectionPool::Wrapper.new(size: 1) { retrieve_connection }
-    end
-  end
-end
-
-# Forces all threads to share the same connection. This works on
-# Capybara because it starts the web server in a thread.
-ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
