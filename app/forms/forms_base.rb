8142f9ab7d341b268e5cb716a53e08b7247e9237
Arthur Dingemans <arthur@argu.co>
2018-08-07 10:39:58 +0200

    [ARGU-1518] Store attr options as hash instead of PropertyShape instances
    So we can pass the form as attribute to PropertyShapes and access form.target in the property shapes

--- app/forms/forms_base.rb
+++ app/forms/forms_base.rb
@@ -41,19 +41,23 @@ class FormsBase
   end
 
   def permit_attribute(attr)
-    return true if _property_groups[attr.model_attribute]
-
+    return true if _property_groups[attr[:model_attribute]]
     permitted_attributes.find do |p_a|
       if p_a.is_a?(Hash)
-        p_a.keys.include?("#{attr.model_attribute}_attributes".to_sym)
+        p_a.keys.include?("#{attr[:model_attribute]}_attributes".to_sym)
       else
-        p_a == attr.model_attribute
+        p_a == attr[:model_attribute]
       end
     end
   end
 
   def permitted_properties
-    @permitted_properties ||= self.class.property_shapes.select(&method(:permit_attribute))
+    @permitted_properties ||=
+      self.class.property_shapes_attrs.select(&method(:permit_attribute)).map(&method(:property_shape))
+  end
+
+  def property_shape(attrs)
+    SHACL::PropertyShape.new(attrs.merge(form: self))
   end
 
   def target_class
@@ -66,16 +70,16 @@ class FormsBase
       target._property_groups = {}
     end
 
-    def property_shapes
-      @property_shapes ||=
+    def property_shapes_attrs
+      @property_shapes_attrs ||=
         _fields
-          .map { |k, attr| property_shape(k, attr || {}) }
+          .map { |k, attr| property_shape_attrs(k, attr || {}) }
           .compact
     end
 
     private
 
-    def ar_attr_to_datatype(attr)
+    def attr_to_datatype(attr)
       return nil if method_defined?(attr.name)
 
       name = attr.name.to_s
@@ -95,13 +99,6 @@ class FormsBase
       else
         NS::XSD[:string] if model_class.defined_enums.key?(name)
       end
-    end
-
-    def attribute_for_key(key)
-      return serializer_attributes[key] if serializer_attributes[key]
-
-      k_v = serializer_reflections.find { |_k, v| (v[:options][:association] || v.name) == key }
-      k_v[1] if k_v
     end
 
     def decimal_data_type(name)
@@ -139,40 +136,33 @@ class FormsBase
     end
 
     def fields(arr, group_iri = nil)
-      arr.each do |field|
-        raise "Resource field '#{field}' defined twice" if _fields[field].present?
-        _fields[field.try(:to_sym)] = {
-          order: _fields.keys.length,
-          group: group_iri
-        }
-      end
+      arr.each { |f| field(f, group: group_iri) }
     end
 
-    def literal_property(attr, pred, instance_props)
-      enum = model_class.defined_enums[attr.name.to_s]&.keys
-      datatype = attr.dig(:options, :datatype) || (enum ? NS::XSD[:string] : ar_attr_to_datatype(attr))
-      m_attr = model_attribute(attr)
+    def field(key, opts = {})
+      raise "Resource field '#{field}' defined twice" if _fields[key].present?
+      opts[:order] = _fields.keys.length
+      _fields[key.try(:to_sym)] = opts
+    end
 
-      SHACL::PropertyShape.new(
-        datatype: datatype,
-        max_count: 1,
-        description: description_for_attr(attr),
-        group: instance_props[:group],
-        model_attribute: m_attr,
-        name: name_for_attr(attr.name),
-        order: instance_props[:order],
-        path: pred,
-        sh_in: enum && RDF::List(enum),
-        **validators_to_sh_props(validators(m_attr))
-      )
+    def literal_property_attrs(attr, attrs)
+      enum = model_enums[attr.name.to_s]
+      attrs[:datatype] = attr.dig(:options, :datatype) || (enum ? NS::XSD[:string] : attr_to_datatype(attr))
+      attrs[:max_count] = 1
+      attrs[:sh_in] = enum && RDF::List(enum)
+      attrs
     end
 
     def model_attribute(attr)
-      (model_class.attribute_alias(attr.name) || attr.name).to_sym
+      (model_class.attribute_alias(attr) || attr).to_sym
     end
 
     def model_class
       @model_class ||= name.sub(/Form$/, '').safe_constantize
+    end
+
+    def model_enums
+      model_class.try(:defined_enums) || {}
     end
 
     def name_for_attr(attr_key)
@@ -180,26 +170,21 @@ class FormsBase
              default: I18n.t("formtastic.labels.#{attr_key}", default: nil))
     end
 
-    def node_property(attr, pred, instance_props)
+    def node_property_attrs(attr, attrs)
       name = attr.dig(:options, :association) || attr.name
       collection = model_class.try(:collections)&.find { |c| c[:name] == name }
       klass_name = model_class.try(:reflections).try(:[], name.to_s)&.class_name || name.to_s.classify
-      m_attr = model_attribute(attr)
 
-      max_count = collection || attr.is_a?(ActiveModel::Serializer::HasManyReflection) ? nil : 1
+      attrs[:description] ||= description_for_attr(attr.name)
+      attrs[:name] ||= name_for_attr(attr.name)
+      attrs[:max_count] ||= collection || attr.is_a?(ActiveModel::Serializer::HasManyReflection) ? nil : 1
+      attrs[:sh_class] ||= klass_name.constantize.iri
+      attrs[:referred_shapes] ||= ["#{klass_name}Form".safe_constantize]
+      attrs
+    end
 
-      SHACL::PropertyShape.new(
-        description: description_for_attr(attr),
-        group: instance_props[:group],
-        max_count: max_count,
-        model_attribute: m_attr,
-        name: name_for_attr(name),
-        order: instance_props[:order],
-        path: pred,
-        sh_class: klass_name.constantize.iri,
-        referred_shapes: ["#{klass_name}Form".safe_constantize],
-        **validators_to_sh_props(validators(m_attr))
-      )
+    def predicate_from_serializer(serializer_attr)
+      serializer_attr&.dig(:options, :predicate)
     end
 
     def property_group(name, opts = {})
@@ -213,45 +198,43 @@ class FormsBase
       fields opts[:properties], group.iri
     end
 
-    def property_shape(attr_key, instance_props = {})
-      attr = attribute_for_key(attr_key)
-      pred = attr&.dig(:options, :predicate)
-      return if pred.blank?
+    def property_shape_attrs(attr_key, attrs = {})
+      return if _property_groups.key?(attr_key)
+      serializer_attr = serializer_attribute(attr_key)
+      attrs[:path] ||= predicate_from_serializer(serializer_attr)
 
-      if attr.is_a?(ActiveModel::Serializer::Attribute)
-        literal_property(attr, pred, instance_props)
-      else
-        node_property(attr, pred, instance_props)
+      attrs[:model_attribute] ||= model_attribute(attr_key)
+      attrs[:validators] ||= validators(attrs[:model_attribute])
+      return attrs if serializer_attr.blank?
+
+      if serializer_attr.is_a?(ActiveModel::Serializer::Attribute)
+        literal_property_attrs(serializer_attr, attrs)
+      elsif serializer_attr.is_a?(ActiveModel::Serializer::Reflection)
+        node_property_attrs(serializer_attr, attrs)
       end
     end
 
+    def serializer_attribute(key)
+      return serializer_attributes[key] if serializer_attributes[key]
+
+      k_v = serializer_reflections.find { |_k, v| (v[:options][:association] || v.name) == key }
+      k_v[1] if k_v
+    end
+
     def serializer_class
-      @serializer_class ||= model_class.serializer_class!
+      @serializer_class ||= model_class.try(:serializer_class!)
     end
 
     def serializer_attributes
-      serializer_class._attributes_data
+      serializer_class&._attributes_data || {}
     end
 
     def serializer_reflections
-      serializer_class._reflections
+      serializer_class&._reflections || {}
     end
 
     def validators(model_attribute)
       model_class.validators.select { |v| v.attributes.include?(model_attribute) }
-    end
-
-    def validators_to_sh_props(validators)
-      opts = {}
-      validators.each do |validator|
-        if validator.is_a?(ActiveRecord::Validations::PresenceValidator)
-          opts[:min_count] = 1
-        elsif validator.is_a?(ActiveRecord::Validations::LengthValidator)
-          opts[:max_length] = validator.options[:maximum]
-          opts[:min_length] = validator.options[:minimum]
-        end
-      end
-      opts
     end
   end
 end
--- app/models/shacl/property_shape.rb
+++ app/models/shacl/property_shape.rb
@@ -2,8 +2,24 @@
 
 module SHACL
   class PropertyShape < Shape
+    class << self
+      def iri
+        NS::SH[:PropertyShape]
+      end
+
+      def validations(*validations)
+        validations.each do |key, klass, option_key|
+          attr_writer key
+
+          define_method key do
+            instance_variable_get(:"@#{key}") || validator_option(klass, option_key)
+          end
+        end
+      end
+    end
+
     # Custom attributes
-    attr_accessor :model_attribute
+    attr_accessor :model_attribute, :form
 
     # SHACL attributes
     attr_accessor :sh_class,
@@ -11,19 +27,24 @@ module SHACL
                   :default_value,
                   :description,
                   :group,
-                  :min_count,
-                  :max_count,
-                  :min_length,
-                  :max_length,
+                  :model_class,
                   :name,
                   :node,
                   :node_kind,
+                  :node_shape,
+                  :max_count,
                   :order,
                   :path,
-                  :sh_in
+                  :validators
 
-    def self.iri
-      NS::SH[:PropertyShape]
+    validations [:min_count, ActiveRecord::Validations::PresenceValidator, :min_count],
+                [:min_length, ActiveRecord::Validations::LengthValidator, :minimum],
+                [:max_length, ActiveRecord::Validations::LengthValidator, :maximum],
+                [:sh_in, ActiveModel::Validations::InclusionValidator, :in]
+
+    def validator_option(klass, option_key)
+      option = validators&.detect { |validator| validator.is_a?(klass) }&.options.try(:[], option_key)
+      option.respond_to?(:call) ? option.call(form.target) : option
     end
   end
 end
