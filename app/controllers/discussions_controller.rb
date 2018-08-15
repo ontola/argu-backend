e752537e3177da909a3ab7b2c94f019977a1c7fe
Arthur Dingemans <arthur@argu.co>
2018-08-15 09:18:21 +0200

    [ARGU-1531] Make discussions filterable on pinned

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -10,14 +10,6 @@ class DiscussionsController < ParentableController
     authorize parent_resource!, :index_children?, controller_name
   end
 
-  def collection_options
-    params
-      .permit(:page)
-      .to_h
-      .merge(user_context: user_context)
-      .to_options
-  end
-
   def new_success
     return respond_with_form(default_form_options(:new)) if active_response_type == :html
     respond_with_resource(
--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -6,12 +6,15 @@ class Discussion
 
   enhance Createable
 
+  include Ldable
   include Iriable
   include Parentable
 
   attr_accessor :forum, :page, :publisher
   parentable :forum, :page
   alias edgeable_record parent
+
+  filterable pinned: {key: :pinned_at, values: {yes: 'NOT NULL', no: 'NULL'}}
 
   def self.default_per_page
     12
