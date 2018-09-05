2988304f259002c042138b8af15ac3251d62ad77
Fletcher91 <thom@argu.co>
2018-09-05 17:48:06 +0200

    [ARGU-1582] Add missing MediaObject, ImageObject, VideoObject properties

--- app/serializers/media_object_serializer.rb
+++ app/serializers/media_object_serializer.rb
@@ -5,11 +5,26 @@ class MediaObjectSerializer < RecordSerializer
 
   attribute :url, predicate: NS::SCHEMA[:url]
   attribute :content, predicate: NS::SCHEMA[:contentUrl]
+  attribute :content_type, predicate: NS::SCHEMA[:encodingFormat]
+  attribute :created_at, predicate: NS::SCHEMA[:uploadDate]
+  attribute :description, predicate: NS::SCHEMA[:caption]
+  attribute :embed_url, predicate: NS::SCHEMA[:embedUrl]
+  attribute :filename, predicate: NS::DBO[:filename]
   attribute :thumbnail, predicate: NS::SCHEMA[:thumbnail]
   attribute :position_y, predicate: NS::ARGU[:imagePositionY]
   attribute :used_as
 
+  def content_type
+    object.content_type unless object.type == 'video'
+  end
+
   def type
-    object.is_image? ? NS::SCHEMA[:ImageObject] : NS::SCHEMA[:MediaObject]
+    if object.type == 'image' || object.profile_photo?
+      NS::SCHEMA[:ImageObject]
+    elsif object.type == 'video'
+      NS::SCHEMA[:VideoObject]
+    else
+      NS::SCHEMA[:MediaObject]
+    end
   end
 end
