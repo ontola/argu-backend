3c0c05d560645297f9a3c42a4e9726cd788bd595
Arthur Dingemans <arthur@argu.co>
2018-09-24 11:08:36 +0200

    [ARGU-1609] Use default_vote_event when looking for guest votes

--- app/helpers/redis_resources_helper.rb
+++ app/helpers/redis_resources_helper.rb
@@ -5,8 +5,8 @@ module RedisResourcesHelper
 
   def schedule_redis_resource_worker(old_user, new_user, redirect = nil)
     resource = resource_from_iri(redirect)
-    if resource.present? && resource.is_a?(Edge) && ![Page, Forum].include?(resource.class)
-      RedisResource::Relation.where(publisher: old_user, parent: resource).persist(new_user)
+    if resource.is_a?(Edge) && resource.default_vote_event.present?
+      RedisResource::Relation.where(publisher: old_user, parent: resource.default_vote_event).persist(new_user)
     end
     RedisResourceWorker.perform_async(old_user.class, old_user.id, new_user.class, new_user.id)
   end
