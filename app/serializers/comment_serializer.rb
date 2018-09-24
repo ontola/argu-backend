3cfec4732503c35155aec339abd253f19e7e0725
Arthur Dingemans <arthur@argu.co>
2018-09-24 13:43:15 +0200

    [ARGU-1612] Hide body of trashed/deleted comments

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class CommentSerializer < ContentEdgeSerializer
+  def description
+    object.is_trashed? ? I18n.t('trashed') : object.description || I18n.t('deleted')
+  end
+
   def display_name
     object.title.presence
   end
