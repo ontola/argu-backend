f21b11fca804f379822e229e489bea435145c598
Arthur Dingemans <arthur@argu.co>
2018-10-18 10:55:33 +0200

    [ARGU-1658] Serialize comments count and top comment for commentables

--- app/enhancements/commentable/serializer.rb
+++ app/enhancements/commentable/serializer.rb
@@ -5,7 +5,9 @@ module Commentable
     extend ActiveSupport::Concern
 
     included do
+      count_attribute :comments
       with_collection :comments, predicate: NS::SCHEMA[:comments]
+      has_one :top_comment, predicate: NS::ARGU[:topComment]
     end
   end
 end
