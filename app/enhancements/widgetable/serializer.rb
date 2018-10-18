27b9cd30da49cb084fa08340917f9f32211a2595
Arthur Dingemans <arthur@argu.co>
2018-10-18 11:02:10 +0200

    [ARGU-1658] Update rdf gems

--- app/enhancements/widgetable/serializer.rb
+++ app/enhancements/widgetable/serializer.rb
@@ -4,7 +4,7 @@ module Widgetable
   module Serializer
     extend ActiveSupport::Concern
     included do
-      has_one :widget_sequence, predicate: NS::ARGU[:widgets]
+      has_one :widget_sequence, predicate: NS::ARGU[:widgets], unless: :export_scope?
     end
   end
 end
