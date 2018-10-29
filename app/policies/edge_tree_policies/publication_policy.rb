c1e4f3e0683843322f602f445aae4f8b3b70b6ec
Arthur Dingemans <arthur@argu.co>
2018-10-29 11:00:01 +0100

    [ARGU-1525] Child instances are now published, so also check for new_record? in PublicationPolicy

--- app/policies/edge_tree_policies/publication_policy.rb
+++ app/policies/edge_tree_policies/publication_policy.rb
@@ -4,7 +4,7 @@ class PublicationPolicy < EdgeTreePolicy
   def permitted_attribute_names
     attributes = super
     attributes.concat %i[id]
-    unless record.publishable&.is_published?
+    unless record.publishable&.is_published? && !new_record?
       attributes.concat %i[draft] if new_record?
       attributes.concat %i[published_at] if moderator? || administrator? || staff?
     end
