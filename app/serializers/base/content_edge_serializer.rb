720915b32ddc3aa37f714fb0ec6e75b4fdfaac6d
Arthur Dingemans <arthur@argu.co>
2018-11-02 11:24:43 +0100

    [ARGU-1676] Add followsCount to content edges and motionsCount to questions

--- app/serializers/base/content_edge_serializer.rb
+++ app/serializers/base/content_edge_serializer.rb
@@ -3,4 +3,5 @@
 class ContentEdgeSerializer < EdgeSerializer
   attribute :description, predicate: NS::SCHEMA[:text]
   attribute :pinned, predicate: NS::ARGU[:pinned]
+  attribute :follows_count, predicate: NS::ARGU[:followsCount]
 end
--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -3,6 +3,7 @@
 class QuestionSerializer < ContentEdgeSerializer
   attribute :default_motion_sorting, predicate: NS::ARGU[:defaultSorting]
   attribute :require_location, predicate: NS::ARGU[:requireLocation]
+  count_attribute :motions
 
   enum :default_motion_sorting
 end
