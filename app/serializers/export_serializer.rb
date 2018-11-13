f8156b4ffe36874ab590ef9af1e3d998ae3ae3c4
Arthur Dingemans <arthur@argu.co>
2018-11-13 16:25:56 +0100

    [ARGU-1443] Add link to export_collection to fix expires amazon url issue

--- app/serializers/export_serializer.rb
+++ app/serializers/export_serializer.rb
@@ -5,8 +5,13 @@ class ExportSerializer < RecordSerializer
   attribute :status, predicate: NS::ARGU[:exportStatus]
   has_one :user, predicate: NS::SCHEMA[:creator]
   has_one :edge, predicate: NS::SCHEMA[:object]
+  has_one :export_collection
 
   def download_url
     object.zip.url
+  end
+
+  def export_collection
+    object.edge.export_collection
   end
 end
