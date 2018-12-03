c1ee67007593de22d89b8ce4c51b06ece9f386df
Arthur Dingemans <arthur@argu.co>
2018-12-03 11:07:04 +0100

    [ARGU][TS-54] Convert DiscussionsPolicy to an EdgePolicy

--- app/policies/edge_tree_policies/discussion_policy.rb
+++ app/policies/edge_tree_policies/discussion_policy.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-class DiscussionPolicy < EdgeTreePolicy
+class DiscussionPolicy < EdgePolicy
   def show?
-    edgeable_policy.list?
-    edgeable_policy.show?
+    parent_policy.list?
+    parent_policy.show?
   end
 
   def create?
-    edgeable_policy.list?
+    parent_policy.list?
     motion_policy.create? || question_policy.create?
   end
 
@@ -15,11 +15,11 @@ class DiscussionPolicy < EdgeTreePolicy
 
   def motion_policy
     @motion_policy ||=
-      Pundit.policy(context, child_instance(edgeable_record, Motion))
+      Pundit.policy(context, child_instance(record.parent, Motion))
   end
 
   def question_policy
     @question_policy ||=
-      Pundit.policy(context, child_instance(edgeable_record, Question))
+      Pundit.policy(context, child_instance(record.parent, Question))
   end
 end
