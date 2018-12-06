e56bc9b5f2b426779931de22ee796c969515d8dd
Arthur Dingemans <arthur@argu.co>
2018-12-06 16:37:16 +0100

    [ARGU][TS-55] Move regexHelper to the service_module

--- app/helpers/regex_helper.rb
+++ app/helpers/regex_helper.rb
@@ -1,11 +1,1 @@
-# frozen_string_literal: true
-
-module RegexHelper
-  EMAIL = /
-    \A
-    #{RFC822::Patterns::LOCAL_PT}
-    \x40
-    (?:(?:#{URI::REGEXP::PATTERN::DOMLABEL}\.)+#{URI::REGEXP::PATTERN::TOPLABEL}\.?)+#{RFC822::Patterns::ATOM}
-    \z
-  /x
-end
+../../service_module/app/helpers/regex_helper.rb
