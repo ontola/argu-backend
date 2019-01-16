ab6cabc6137268afc9ca0460a2ab1df3f876aec8
Arthur Dingemans <arthur@argu.co>
2019-01-16 11:48:44 +0100

    [ARGU-1724] Remove unused model Rule

--- app/models/rule.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Rule < ApplicationRecord
-  # @todo remove this association and its columns after migration
-  belongs_to :context, polymorphic: true, inverse_of: :rules
-  belongs_to :branch, class_name: 'Edge'
-  belongs_to :model, polymorphic: true, inverse_of: :rules
-  enum trickles: {doesnt_trickle: 0, trickles_down: 1, trickles_up: 2}
-end
