d3270795cd7e103ce57af1260ac78ca1287a6cfb
Arthur Dingemans <arthur@argu.co>
2019-01-23 16:16:14 +0100

    [ARGU-1724] Redirect some of the old url's

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -8,17 +8,23 @@ class RedirectController < ApplicationController
   private
 
   def resource # rubocop:disable Metrics/AbcSize
-    case params[:resource]
-    when 'Decision'
-      Edge
-        .find_by!(owner_id: params[:id], owner_type: 'Motion')
-        .decisions
-        .find_by(step: params[:step])
-    when 'Argument'
-      Edge.find_by(owner_id: params[:id], owner_type: 'ConArgument') ||
-        Edge.find_by!(owner_id: params[:id], owner_type: 'ProArgument')
-    else
-      Edge.find_by!(owner_id: params[:id], owner_type: params[:resource])
+    ActsAsTenant.without_tenant do
+      case params[:resource]
+      when 'Decision'
+        Edge
+          .find_by!(owner_id: params[:id], owner_type: 'Motion')
+          .decisions
+          .find_by(step: params[:step])
+      when 'Argument'
+        Edge.find_by(owner_id: params[:id], owner_type: 'ConArgument') ||
+          Edge.find_by!(owner_id: params[:id], owner_type: 'ProArgument')
+      else
+        Edge.find_by!(owner_id: params[:id], owner_type: params[:resource])
+      end
     end
+  end
+
+  def resource_by_shortname
+    params[:shortname] && Edge.find_via_shortname!(params[:shortname])
   end
 end
