8849804c12cac03c8ef80ca827f6b17d84afe3a2
Arthur Dingemans <arthur@argu.co>
2019-03-05 16:54:59 +0100

    [ARGU-1527] Line length

--- app/enhancements/cover_photoable/policy.rb
+++ app/enhancements/cover_photoable/policy.rb
@@ -6,8 +6,9 @@ module CoverPhotoable
 
     def permitted_attribute_names
       attributes = super
+      photo_attrs = Pundit.policy(context, MediaObject.new(about: record)).permitted_attributes
       attributes.append(
-        default_cover_photo_attributes: Pundit.policy(context, MediaObject.new(about: record)).permitted_attributes
+        default_cover_photo_attributes: photo_attrs
       )
       attributes
     end
