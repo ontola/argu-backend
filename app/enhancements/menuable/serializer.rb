5666baa58dba47ef866d493340732a02c82662b3
Arthur Dingemans <arthur@argu.co>
2019-03-08 17:38:02 +0100

    [ARGU-1725] Fix inheritance of menus

--- app/enhancements/menuable/serializer.rb
+++ app/enhancements/menuable/serializer.rb
@@ -9,7 +9,14 @@ module Menuable
     end
 
     module ClassMethods
+      def inherited(target)
+        target.include_menus
+        super
+      end
+
       def include_menus # rubocop:disable Metrics/AbcSize
+        return if name.blank?
+
         "#{name.gsub('Serializer', '')}MenuList".constantize.defined_menus.each do |menu|
           method_name = "#{menu}_menu"
           define_method method_name do
