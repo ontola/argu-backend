f56c814ef3d81d5919032306d942ad59c7c8ebb7
Arthur Dingemans <arthur@argu.co>
2019-03-11 13:37:45 +0100

    [ARGU-1527] Add _destroy to serializers

--- app/serializers/base/record_serializer.rb
+++ app/serializers/base/record_serializer.rb
@@ -5,6 +5,7 @@ class RecordSerializer < BaseSerializer
   attribute :created_at, predicate: NS::SCHEMA[:dateCreated]
   attribute :published_at, predicate: NS::SCHEMA[:datePublished]
   attribute :display_name, predicate: NS::SCHEMA[:name], graph: NS::LL[:add]
+  attribute :_destroy, predicate: NS::ONTOLA[:_destroy]
 
   def export?
     scope&.doorkeeper_scopes&.include? 'export'
