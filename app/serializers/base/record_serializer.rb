b9c7e4235a27f8927402dea51ef0cd5e189aecde
Arthur Dingemans <arthur@argu.co>
2019-03-18 16:44:29 +0100

    [ARGU-1759] Don't serialize _destroy property

--- app/serializers/base/record_serializer.rb
+++ app/serializers/base/record_serializer.rb
@@ -5,7 +5,7 @@ class RecordSerializer < BaseSerializer
   attribute :created_at, predicate: NS::SCHEMA[:dateCreated]
   attribute :published_at, predicate: NS::SCHEMA[:datePublished]
   attribute :display_name, predicate: NS::SCHEMA[:name], graph: NS::LL[:add]
-  attribute :_destroy, predicate: NS::ONTOLA[:_destroy]
+  attribute :_destroy, predicate: NS::ONTOLA[:_destroy], if: :never
 
   def export?
     scope&.doorkeeper_scopes&.include? 'export'
