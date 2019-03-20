56f029eba3d7c36629ceed05fe661443addb1c55
Arthur Dingemans <arthur@argu.co>
2019-03-20 11:48:03 +0100

    [ARGU-1767] Fix staff feed

--- app/controllers/favorites_feed_controller.rb
+++ app/controllers/favorites_feed_controller.rb
@@ -16,5 +16,24 @@ class FavoritesFeedController < FeedController
     current_user
   end
 
+  def index_collection
+    collection = super
+    collection.instance_variable_set(
+      :@iri,
+      RDF::URI(DynamicUriHelper.rewrite(path_with_hostname('/staff/feed'), argu_page))
+    )
+    collection
+  end
+
   def parent_resource; end
+
+  def set_tenant_header
+    super
+    ActsAsTenant.current_tenant = nil
+    @tree_root = nil
+  end
+
+  def argu_page
+    @argu_page ||= Page.find_via_shortname('argu')
+  end
 end
