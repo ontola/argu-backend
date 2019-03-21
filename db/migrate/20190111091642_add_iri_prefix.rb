b56d2e1cf7e46ee108ff4476f93bf49196d37728
Arthur Dingemans <arthur@argu.co>
2019-03-21 12:12:12 +0100

    [ARGU-1754] Improve speed of migration

--- db/migrate/20190111091642_add_iri_prefix.rb
+++ db/migrate/20190111091642_add_iri_prefix.rb
@@ -1,8 +1,11 @@
 class AddIRIPrefix < ActiveRecord::Migration[5.2]
   def change
-    Page.joins(:shortname).find_each do |page|
-      page.update(iri_prefix: "app.argu.co/#{page.url}")
-    end
+    Page.connection.update(
+      'INSERT INTO properties (created_at, updated_at, edge_id, predicate, string) ('\
+        "SELECT CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, edges.uuid, 'https://argu.co/ns/core#iriPrefix', 'app.argu.co/' || shortnames.shortname FROM edges CROSS JOIN shortnames "\
+        "WHERE edges.owner_type = 'Page' AND shortnames.owner_type = 'Edge' AND edges.uuid = shortnames.owner_id AND shortnames.primary = true"\
+      ')'
+    )
 
     # Drop unused tables
     drop_table :sources
--- db/migrate/20190129132716_reset_iris.rb
+++ db/migrate/20190129132716_reset_iris.rb
@@ -2,6 +2,5 @@ class ResetIris < ActiveRecord::Migration[5.2]
   def change
     Edge.update_all(iri_cache: nil)
     Widget.destroy_all
-    Forum.find_each { |f| f.send(:create_default_widgets) }
   end
 end
