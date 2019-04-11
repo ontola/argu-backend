dcd6601c99ead7d86b088c7344344c4052f9de60
Arthur Dingemans <arthur@argu.co>
2019-04-11 14:33:13 +0200

    [ARGU-1795] Add theme to the page form

---
+++ app/enhancements/stylable/policy.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Stylable
+  module Policy
+    extend ActiveSupport::Concern
+
+    def permitted_attribute_names
+      attributes = super
+      attributes.append(%i[navbar_color navbar_background accent_color accent_background_color])
+      attributes
+    end
+  end
+end
--- app/forms/page_form.rb
+++ app/forms/page_form.rb
@@ -4,5 +4,16 @@ class PageForm < ApplicationForm
   fields %i[
     visibility
     url
+    theme
   ]
+
+  property_group :theme,
+                 label: -> { I18n.t('forms.theme.label') },
+                 description: -> { I18n.t('forms.theme.description') },
+                 properties: %i[
+                   navbar_color
+                   navbar_background
+                   accent_color
+                   accent_background_color
+                 ]
 end
