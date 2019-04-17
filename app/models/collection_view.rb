d6ff07601cc87295ec3af803625e5d988f8026e7
Arthur Dingemans <arthur@argu.co>
2019-04-17 13:03:13 +0200

    [ARGU-1827] Add edges from collection to the grant tree

--- app/models/collection_view.rb
+++ app/models/collection_view.rb
@@ -1,1 +1,11 @@
-../../service_module/app/models/collection_view.rb
+# frozen_string_literal: true
+
+class CollectionView < RailsLD::CollectionView
+  def members
+    m = super
+    if association_class <= Edge && association_class != Page
+      m.each { |member| user_context.grant_tree.cache_node(member) }
+    end
+    m
+  end
+end
