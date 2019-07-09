b3fb15dbe26dfda670047fc8fc6e871b66a0b05a
Arthur Dingemans <arthur@argu.co>
2019-07-09 17:45:32 +0200

    [ARGU-1864] Add max to motion#description

--- app/models/motion.rb
+++ app/models/motion.rb
@@ -19,6 +19,7 @@ class Motion < Discussion
   paginates_per 30
   parentable :question, :container_node
 
+  validates :description, length: {maximum: 5000}
   validates :display_name, presence: true, length: {maximum: 110}
   validates :title, presence: true
   validates :creator, presence: true
