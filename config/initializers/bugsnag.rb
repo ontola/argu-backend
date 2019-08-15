11d6fa111c4f09ca11f5204635a8262487416077
Arthur Dingemans <arthur@argu.co>
2019-08-15 13:17:20 +0200

    [ARGU][RIVM-1] Add tenant tab to bugsnag

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -7,3 +7,12 @@ Bugsnag.configure do |config|
   config.ignore_classes.delete('ActionController::InvalidAuthenticityToken')
   config.ignore_user_agents << /pagefreezer/
 end
+
+Bugsnag.before_notify_callbacks << lambda { |report|
+  report.add_tab(
+    :tenant,
+    schema: Apartment::Tenant.current,
+    tenant: ActsAsTenant.current_tenant&.url,
+    tenant_id: ActsAsTenant.current_tenant&.uuid
+  )
+}
