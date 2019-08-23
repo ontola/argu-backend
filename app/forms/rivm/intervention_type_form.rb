a492ec81896b79fcc9199ec075f67263daa91bfc
Arthur Dingemans <arthur@argu.co>
2019-08-23 14:23:46 +0200

    [ARGU][RIVM-2] Set default value for risk

--- app/forms/rivm/intervention_type_form.rb
+++ app/forms/rivm/intervention_type_form.rb
@@ -8,7 +8,8 @@ class InterventionTypeForm < ApplicationForm
       example_of_id: {
         max_count: 99,
         sh_in: -> { iri_from_template(:risks_collection_iri, page: 1, page_size: 100, fragment: :members) },
-        datatype: NS::XSD[:string]
+        datatype: NS::XSD[:string],
+        default_value: -> { [target.parent&.iri] }
       }
     },
     :attachments
