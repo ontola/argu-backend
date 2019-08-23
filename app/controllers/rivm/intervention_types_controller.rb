1bfa5f0404f6bb3bd3661d821ab5f6f6d6ec0c92
Arthur Dingemans <arthur@argu.co>
2019-08-23 15:11:46 +0200

    [ARGU][RIVM-2] Improve InterventionType navigation

--- app/controllers/rivm/intervention_types_controller.rb
+++ app/controllers/rivm/intervention_types_controller.rb
@@ -2,4 +2,10 @@
 
 class InterventionTypesController < EdgeableController
   skip_before_action :check_if_registered, only: :index
+
+  private
+
+  def create_service_parent
+    ActsAsTenant.current_tenant
+  end
 end
--- app/serializers/rivm/intervention_type_serializer.rb
+++ app/serializers/rivm/intervention_type_serializer.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class InterventionTypeSerializer < ContentEdgeSerializer
+  private
+
+  def parent
+    Dashboard.find_via_shortname('maatregelen')
+  end
 end
