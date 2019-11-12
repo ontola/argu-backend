72e63aa14bae26d2fbc13da7a020ea1c4b5741bf
Arthur Dingemans <arthur@argu.co>
2019-11-12 11:28:22 +0100

    [ARGU][AOD-654] Fix stats page

--- app/serializers/data_cube/observation_serializer.rb
+++ app/serializers/data_cube/observation_serializer.rb
@@ -6,11 +6,15 @@ module DataCube
     triples :measures
 
     def dimensions
-      object.dimensions.map { |dimension, value| RDF::Statement.new(object.iri, dimension.predicate, value) }
+      object.dimensions.map do |dimension, value|
+        RDF::Statement.new(object.iri, dimension.predicate, value, graph_name: NS::LL[:supplant])
+      end
     end
 
     def measures
-      object.measures.map { |measure, value| RDF::Statement.new(object.iri, measure.predicate, value) }
+      object.measures.map do |measure, value|
+        RDF::Statement.new(object.iri, measure.predicate, value, graph_name: NS::LL[:supplant])
+      end
     end
   end
 end
