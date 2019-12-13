b4e6c4cbd1f4b25c40733326d5d8c8a958494cb4
Arthur Dingemans <arthur@argu.co>
2019-12-13 10:17:57 +0100

    [ARGU-1603] Fix argument counts in statistics

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -62,7 +62,7 @@ class StatisticsController < ParentableController
       NS::ARGU[:questionsCount] => counts['Question'] || 0,
       NS::ARGU[:motionsCount] => counts['Motion'] || 0,
       NS::ARGU[:commentsCount] => counts['Comment'] || 0,
-      NS::ARGU[:argumentsCount] => counts['Argument'] || 0
+      NS::ARGU[:argumentsCount] => [counts['Argument'], counts['ProArgument'], counts['ConArgument']].compact.sum
     }
     @observation_measures[NS::ARGU[:contributionsCount]] =
       contribution_keys.reduce(0) { |sum, key| sum + @observation_measures[key] }
