1038dcc9b1ad223a8d0662aee4ac16974f9ec822
Arthur Dingemans <arthur@argu.co>
2020-03-05 10:22:33 +0100

    core#1 Upgrade dependencies

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-Sidekiq.logger.formatter = Sidekiq::Logging::Json::Logger.new if Rails.env.development?
-
 Sidekiq.configure_server do |config|
   config.redis = {
     url: ENV['REDIS_URL'].presence || "redis://#{ENV['REDIS_ADDRESS'] || 'localhost'}:#{ENV['REDIS_PORT'] || 6379}/12"
