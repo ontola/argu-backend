cc7c7df5736e69744d6dd4d25f0db02b9920c438
Arthur Dingemans <arthur@argu.co>
2020-03-09 12:19:27 +0100

    core#237 Fix sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
 Sidekiq.configure_server do |config|
   config.redis = {
     url: ENV['REDIS_URL'].presence || "redis://#{ENV['REDIS_ADDRESS'] || 'localhost'}:#{ENV['REDIS_PORT'] || 6379}/12"
   }
+end
+
+module Sidekiq
+  module WebHelpers
+    def root_path
+      "#{ActsAsTenant.current_tenant.iri.path}#{env['SCRIPT_NAME']}/"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'sidekiq/prometheus/exporter'
-require 'sidekiq/web'
 
 ####
 # Routes
