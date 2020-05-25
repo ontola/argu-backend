b36e02fde88d17fbfe8274dffb565cbd81a8d50e
Arthur Dingemans <arthur@argu.co>
2020-05-25 16:18:33 +0200

    RIVM-75 Enable banner tab for admins

--- app/policies/edgeable_policies/page_policy.rb
+++ app/policies/edgeable_policies/page_policy.rb
@@ -18,8 +18,8 @@ class PagePolicy < EdgePolicy
 
   def permitted_tabs
     tabs = []
-    tabs.concat %i[general profile container_nodes groups shortnames]
-    tabs.concat %i[custom_menu_items banners] if staff?
+    tabs.concat %i[general profile container_nodes groups shortnames banners]
+    tabs.concat %i[custom_menu_items] if staff?
     tabs
   end
 
