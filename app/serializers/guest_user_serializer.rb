57578b0e5375b4c2dea172608fe0463b8a4fefd3
Arthur Dingemans <arthur@argu.co>
2020-07-21 12:07:44 +0200

    core#5 Serialize language of guest users

--- app/serializers/guest_user_serializer.rb
+++ app/serializers/guest_user_serializer.rb
@@ -2,6 +2,9 @@
 
 class GuestUserSerializer < BaseSerializer
   attribute :display_name, predicate: NS::SCHEMA[:name]
+  attribute :language, predicate: NS::SCHEMA[:language] do
+    UserSerializer.enum_options(:language)[I18n.locale]&.iri
+  end
 
   def self.self?(object, opts)
     object == opts[:scope]&.user
