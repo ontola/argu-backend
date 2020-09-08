ebcb0922176fb765f47800eab77127b79bc8722a
Fletcher91 <thom@argu.co>
2020-09-08 14:27:33 +0200

    core#412 Validate against the current tenant rather than a static list

--- app/helpers/redirect_helper.rb
+++ app/helpers/redirect_helper.rb
@@ -1,18 +1,22 @@
 # frozen_string_literal: true
 
 module RedirectHelper
-  VALID_HOSTNAMES = [
-    'https://argu.co',
-    "https://#{Rails.application.config.host_name}",
-    Rails.env.test? ? 'https://127.0.0.1:42000' : nil
-  ].uniq.compact.freeze
-
   def argu_iri_or_relative?(redirect)
     uri = redirect && URI.parse(redirect)
     return true if uri.nil? || uri.hostname.nil?
 
     uri.scheme = 'https' if Rails.env.test?
     port = [nil, 80, 443].include?(uri.port) ? '' : ":#{uri.port}"
-    VALID_HOSTNAMES.include?("#{uri.scheme}://#{uri.hostname}#{port}")
+    origin = "#{uri.scheme}://#{uri.hostname}#{port}"
+
+    valid_hosts.include?(origin)
+  end
+
+  def valid_hosts
+    [
+      ActsAsTenant.current_tenant&.iri,
+      "https://#{Rails.application.config.host_name}",
+      Rails.env.test? ? 'https://127.0.0.1:42000' : nil
+    ].compact
   end
 end
