d1ce6042162ace345071e2749d59aa7f79551cc0
Arthur Dingemans <arthur@argu.co>
2020-11-17 14:22:12 +0100

    core#463 Remove #default_publication_follow_type overwrite for blog posts
    Not doing anything anyway

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 class BlogPostsController < EdgeableController
-  private
-
-  def default_publication_follow_type
-    return super if parent_resource!.is_a?(Page)
-
-    'news'
-  end
 end
