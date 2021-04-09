5123b870f17a06cbbb0952ea4b856928646f958e
Arthur Dingemans <arthur@argu.co>
2021-04-09 14:09:41 +0200

    core#223 Auto follow ordered products

--- app/models/edges/order_detail.rb
+++ app/models/edges/order_detail.rb
@@ -5,6 +5,13 @@ class OrderDetail < Edge
 
   property :offer_id, :linked_edge_id, NS::SCHEMA.orderedItem
   belongs_to :offer, foreign_key_property: :offer_id, class_name: 'Edge', dependent: false
+  after_create :follow_product
+
+  private
+
+  def follow_product
+    publisher.follow(offer.product, :news, :news)
+  end
 
   class << self
     def iri
