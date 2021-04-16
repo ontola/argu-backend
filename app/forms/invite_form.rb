33b0338f75a14fab91bda10a13fbbf32b89ca0f4
Arthur Dingemans <arthur@argu.co>
2021-04-16 16:52:04 +0200

    core#276 Add multiple emails input

--- app/forms/invite_form.rb
+++ app/forms/invite_form.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
+require 'input_fields/multiple_email_input'
+
 class InviteForm < ApplicationForm
   include RegexHelper
 
-  field :addresses, max_length: 5000, pattern: /\A(#{RegexHelper::SINGLE_EMAIL.source},?\s?)+\z/
+  field :addresses,
+        input_field: MultipleEmailInput,
+        max_length: 5000,
+        pattern: /\A(#{RegexHelper::SINGLE_EMAIL.source},?\s?)+\z/
   field :message, max_length: 5000
   field :group_id, sh_in: -> { collection_iri(nil, :groups) }
   field :redirect_url
---
+++ lib/input_fields/multiple_email_input.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class MultipleEmailInput < LinkedRails::Form::Field
+end
