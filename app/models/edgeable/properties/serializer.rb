512a9169bd764e8acdf4889ea85db0c43591ff19
Arthur Dingemans <arthur@argu.co>
2021-06-10 12:18:51 +0200

    core#586 Don't overwrite properties when already defined

--- app/models/edgeable/properties/serializer.rb
+++ app/models/edgeable/properties/serializer.rb
@@ -22,21 +22,29 @@ module Edgeable
 
         private
 
-        def association_property(klass, property_options)
+        def association_property(klass, property_options) # rubocop:disable Metrics/AbcSize
           if property_options[:array]
             name = property_options[:name].to_s.chomp('_ids').pluralize
+            return if klass.attributes_to_serialize.include?(name.to_sym)
+
             klass.has_many name, predicate: property_options[:predicate]
           else
             name = property_options[:name].to_s.chomp('_id')
+            return if klass.attributes_to_serialize.include?(name.to_sym)
+
             klass.has_one name, predicate: property_options[:predicate]
           end
         end
 
         def attribute_property(klass, property_options)
+          return if klass.attributes_to_serialize.include?(property_options[:name].to_sym)
+
           klass.attribute property_options[:name], predicate: property_options[:predicate]
         end
 
         def enum_property(klass, property_options)
+          return if klass.attributes_to_serialize.include?(property_options[:name].to_sym)
+
           klass.enum property_options[:name], predicate: property_options[:predicate]
         end
       end
