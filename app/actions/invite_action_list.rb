f02db9dbd700faeed84939bf98db44f019ad8a8f
Arthur Dingemans <arthur@argu.co>
2021-07-01 18:03:27 +0200

    core#738 Update invite token iri

--- app/actions/invite_action_list.rb
+++ app/actions/invite_action_list.rb
@@ -4,6 +4,6 @@ class InviteActionList < ApplicationActionList
   has_collection_create_action(
     description: -> { I18n.t('tokens.discussion.description') },
     label: -> { I18n.t('tokens.discussion.title') },
-    url: -> { iri_from_template(:tokens_iri) }
+    url: -> { LinkedRails.iri(path: 'tokens/email') }
   )
 end
