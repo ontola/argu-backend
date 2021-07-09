4109aa24b06c3dafe34aeab26d646d4551b706bd
Arthur Dingemans <arthur@argu.co>
2021-07-09 14:55:42 +0200

    core#12 Fix version check in Invalidate Cache Worker

--- app/workers/invalidate_cache_worker.rb
+++ app/workers/invalidate_cache_worker.rb
@@ -6,11 +6,11 @@ class InvalidateCacheWorker
   def perform(current_version, reindex_search = true)
     current_cache_version = Argu::Redis.get('argu.cache.version')
 
-    return if current_version.positive? && current_cache_version >= current_version
+    return if current_version.is_a?(String) && current_cache_version >= current_version
 
     Argu::Cache.invalidate_all
 
-    return unless current_version.positive?
+    return unless current_version.is_a?(String)
 
     Page.reindex_with_tenant unless reindex_search
     Argu::Redis.set('argu.cache.version', current_version)
