802d1e66abb7467cb9e6efd77e8fe097931af80b
Arthur Dingemans <arthur@argu.co>
2021-09-24 15:59:04 +0200

    core#3 Cleanup unused policies

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,16 +3,9 @@
 class StaticPagesController < AuthorizedController
   include UriTemplateHelper
 
-  skip_before_action :authorize_action, only: :not_found
   skip_after_action :verify_authorized, only: :not_found
 
   def not_found
     handle_error(ActionController::RoutingError.new('Route not found'))
-  end
-
-  private
-
-  def authorize_action
-    authorize :static_page
   end
 end
