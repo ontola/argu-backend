c589f6350710d61328f41eec62e37791b84485a1
Job Eijdems <job@argu.co>
2021-11-30 10:21:33 +0100

    core#739 Translate yaml files to German

--- config/application.rb
+++ config/application.rb
@@ -96,8 +96,8 @@ module Argu
     ############################
 
     config.time_zone = 'UTC'
-    I18n.available_locales = %i[nl en]
-    config.i18n.available_locales = %i[nl en]
+    I18n.available_locales = %i[de nl en]
+    config.i18n.available_locales = %i[de nl en]
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
     config.i18n.enforce_available_locales = true
     I18n.enforce_available_locales = true
