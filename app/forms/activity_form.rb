c875dd2fa5b6f5a82095e6d142d9cb0b3facd1d2
Arthur Dingemans <arthur@argu.co>
2021-12-07 16:49:38 +0100

    core#12 Some more translation fixes

--- app/forms/activity_form.rb
+++ app/forms/activity_form.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ActivityForm < ApplicationForm
-  field :comment, input_field: LinkedRails::Form::Field::TextAreaInput
+  field :comment,
+        input_field: LinkedRails::Form::Field::TextAreaInput,
+        label: ''
   field :notify
 end
