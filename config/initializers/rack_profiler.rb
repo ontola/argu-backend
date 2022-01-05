6f4884ea47cb11128136d41eb2ddb4a5bfda9c3f
Arthur Dingemans <arthur@argu.co>
2022-01-05 17:44:16 +0100

    core#2 Disable profiling by default

--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
-if Rails.env.development? || Rails.env.staging?
+# To enable profiling:
+# * Add ENABLE_PROFILING=true to your env
+# * Add unicorn-rails to the gemfile
+# * Add `?pp=flamegraph` to the url you are visiting
+if ENV['ENABLE_PROFILING'] == 'true'
   require 'rack-mini-profiler'
 
   Rack::MiniProfiler.config.start_hidden = true
   # initialization is skipped so trigger it
   Rack::MiniProfilerRails.initialize!(Rails.application)
+  Rack::MiniProfiler.config.authorization_mode = :allow_all
 end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
-app_dir = '/usr/src/app'
-
-working_directory app_dir
-
-pid "#{app_dir}/tmp/unicorn.pid"
-
-worker_processes 2
+worker_processes 1
 
 listen 3000, tcp_nopush: false
