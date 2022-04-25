e7361bdb7f422aeab610a0fe8a59e32c4e75a969
Thom van Kalkeren <thom@argu.co>
2022-04-25 09:57:28 +0200

    core#10 Ensure logging works during initialization

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,11 @@
 # Load the Rails application.
 require_relative 'application'
 
+# Ensure logging works during initialization
+if !Rails.env.test? && (Rails.env.development? || ENV['RAILS_LOG_TO_STDOUT'].present?)
+  Rails.logger = ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new($stdout))
+end
+
 # Initialize the Rails application.
 Rails.application.initialize!
 
