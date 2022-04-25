942c512908d740d18126ba208b188cf0705ca75c
Thom van Kalkeren <thom@argu.co>
2022-04-25 09:59:08 +0200

    core#10 Don't crash the server if cache invalidation fails

--- config/initializers/invalidate_cache.rb
+++ config/initializers/invalidate_cache.rb
@@ -3,5 +3,9 @@
 if Rails.env.development? || ENV['INVALIDATE_CACHE_ON_BOOT']
   require_relative '../../app/workers/invalidate_cache_worker'
 
-  InvalidateCacheWorker.perform_async(-1, reindex_search: false)
+  begin
+    InvalidateCacheWorker.perform_async(-1, reindex_search: false)
+  rescue Redis::CannotConnectError
+    Rails.logger.warn 'Cannot connect to Redis, skipping cache invalidation'
+  end
 end
