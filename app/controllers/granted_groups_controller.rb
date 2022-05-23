e46942159f262556ea84dc3fed388eddb2ad6b4f
Arthur Dingemans <arthur@argu.co>
2022-05-23 16:18:29 +0200

    core#12 Minor fixes

--- app/controllers/granted_groups_controller.rb
+++ app/controllers/granted_groups_controller.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class GrantedGroupsController < AuthorizedController
+  private
+
+  def verify_policy_scoped?
+    false
+  end
+
   class << self
     # GrantedGroups doesn't have a backing model which provides this method.
     def controller_class
