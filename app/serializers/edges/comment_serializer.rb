bfb35223f907a714284fbc986cf6ea8ccd72bdd1
Arthur Dingemans <arthur@argu.co>
2022-05-25 14:04:01 +0200

    core#1084 Set custom breadcrumb for comments

--- app/serializers/edges/comment_serializer.rb
+++ app/serializers/edges/comment_serializer.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class CommentSerializer < ContentEdgeSerializer
+  attribute :breadcrumb, predicate: NS.ontola[:breadcrumb] do |object|
+    RDF::URI("#{object.parent.iri}#comments") if object.parent
+  end
   attribute :description, predicate: NS.schema.text do |object|
     object.is_trashed? ? I18n.t('trashed') : object.description
   end
