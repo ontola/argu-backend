4b371bb51eb7bfa9f8d232b461aabbb4d67a114e
Arthur Dingemans <arthur@argu.co>
2022-08-22 09:26:50 +0200

    fix: add swipetool image

--- app/controllers/edges/swipe_tools_controller.rb
+++ app/controllers/edges/swipe_tools_controller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class SwipeToolsController < SurveysController
+  has_collection_create_action(
+    svg: RDF::URI('https://dptr8y9slmfgv.cloudfront.net/argu/SwipeTool.svg')
+  )
 end
