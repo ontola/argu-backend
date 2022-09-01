5faaf8599a73e42d4d46a26e9b1a0a028e0d7fcd
Arthur Dingemans <arthur@argu.co>
2022-09-01 13:42:08 +0200

    fix: completely tenantize activity mails argu#1257

--- app/workers/send_activity_notifications_worker.rb
+++ app/workers/send_activity_notifications_worker.rb
@@ -1,24 +1,24 @@
 # frozen_string_literal: true
 
-class SendActivityNotificationsWorker
+class SendActivityNotificationsWorker # rubocop:disable Metrics/ClassLength
   include Sidekiq::Worker
 
   COOLDOWN_PERIOD = 4.minutes
+
+  attr_reader :user
 
   # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
   def perform(user_id, delivery_type)
     ActsAsTenant.without_tenant do
       @user = User.find(user_id)
 
-      return if wrong_delivery_type?(delivery_type)
+      return if wrong_delivery_type?(delivery_type) || inside_cooldown_period
 
       ActiveRecord::Base.transaction do
-        collect_activity_notifications
-        if @notifications.length.zero?
-          logger.warn 'No notifications to send'
+        if notifications.exists?
+          send_activity_notifications_mail
         else
-          logger.info "Preparing to possibly send #{@notifications.length} notifications"
-          send_activity_notifications_mail if outside_cooldown_period
+          logger.warn 'No notifications to send'
         end
       rescue ActiveRecord::StatementInvalid => e
         logger.error 'Queue collision occurred' if e.message.include? 'LockNotAvailable'
@@ -30,24 +30,64 @@ class SendActivityNotificationsWorker
 
   private
 
-  def add_prepared_notification(result, notification) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+  def add_prepared_notification(result, notification)
     activity = notification.activity
     trackable = activity.trackable
     followable = activity.new_content? ? activity.recipient : trackable
-    return if followable.nil?
+    return result if followable.nil?
 
-    result[followable.id] ||= Struct::Follow.new(
-      ActsAsTenant.with_tenant(followable.root) { @user.follow_for(followable)&.iri },
-      {display_name: followable.root.display_name},
-      {
-        id: followable.iri,
-        display_name: followable.display_name,
-        pro: followable.try(:pro),
-        type: followable.owner_type
-      },
-      []
-    )
-    result[followable.id].notifications << {
+    result[followable.id] ||= follow_struct(followable)
+    result[followable.id].notifications << prepared_notification(activity, trackable)
+    result
+  end
+
+  def follow_struct(followable)
+    follow_id = user.follow_for(followable)&.iri
+    followable = {
+      id: followable.iri,
+      display_name: followable.display_name,
+      pro: followable.try(:pro),
+      type: followable.owner_type
+    }
+    notifications = []
+    Struct::Follow.new(follow_id, followable, notifications)
+  end
+
+  def fresh_notifications_query
+    Notification.arel_table[:read_at].eq(nil)
+      .and(Notification.arel_table[:created_at].gt(user.notifications_viewed_at || 1.year.ago))
+  end
+
+  def notifications
+    @notifications ||=
+      user
+        .notifications
+        .for_activity
+        .where(fresh_notifications_query)
+        .order(created_at: :desc)
+        .lock('FOR UPDATE NOWAIT')
+  end
+
+  def inside_cooldown_period
+    last_viewed = user.notifications_viewed_at
+
+    last_viewed.present? && last_viewed > (Time.current - COOLDOWN_PERIOD)
+  end
+
+  def joined_notifications
+    notifications
+      .where(root: ActsAsTenant.current_tenant)
+      .includes(
+        activity: [
+          :recipient,
+          :trackable,
+          {owner: [profileable: :default_profile_photo]}
+        ]
+      )
+  end
+
+  def prepared_notification(activity, trackable) # rubocop:disable Metrics/MethodLength
+    {
       action: activity.action,
       content: activity.comment || trackable.content,
       id: trackable.iri,
@@ -62,54 +102,27 @@ class SendActivityNotificationsWorker
     }
   end
 
-  def collect_activity_notifications # rubocop:disable Metrics/MethodLength
-    t_notifications = Notification.arel_table
-    @notifications =
-      @user
-        .notifications
-        .for_activity
-        .where(t_notifications[:read_at]
-                 .eq(nil)
-                 .and(t_notifications[:created_at]
-                        .gt(@user.notifications_viewed_at || 1.year.ago)))
-        .order(created_at: :desc)
-        .lock('FOR UPDATE NOWAIT')
-  end
-
-  def outside_cooldown_period
-    last_viewed = @user.reload.notifications_viewed_at
-    (last_viewed.blank? || last_viewed && (last_viewed < (Time.current - COOLDOWN_PERIOD)))
-  end
-
-  def prepared_notifications # rubocop:disable Metrics/MethodLength
-    result = {}
-    @notifications
-      .includes(
-        activity: [
-          :recipient,
-          :trackable,
-          {owner: [profileable: :default_profile_photo]}
-        ]
-      )
-      .each { |notification| add_prepared_notification(result, notification) }
-    result.transform_values(&:to_h)
+  def prepared_notifications
+    joined_notifications.reduce({}) do |result, notification|
+      add_prepared_notification(result, notification)
+    end.transform_values(&:to_h)
   end
 
   def send_activity_notifications_mail
-    logger.info "Sending #{@notifications.length} notification(s) to #{@user.email}"
-    notifications = prepared_notifications
-    ActsAsTenant.with_tenant(Page.find_by(uuid: @notifications.uniq.pluck(:root_id).first)) do
-      Argu::API
-        .new
-        .create_email(:activity_notifications, @user, follows: notifications)
+    notifications.pluck(:root_id).uniq.each do |root_id|
+      ActsAsTenant.with_tenant(Page.find_by(uuid: root_id)) do
+        Argu::API
+          .new
+          .create_email(:activity_notifications, user, follows: prepared_notifications)
+      end
     end
-    @user.update_column(:notifications_viewed_at, Time.current) # rubocop:disable Rails/SkipsModelValidations
+    user.update_column(:notifications_viewed_at, Time.current) # rubocop:disable Rails/SkipsModelValidations
   end
 
   def wrong_delivery_type?(delivery_type)
-    return false if delivery_type.present? && User.reactions_emails[@user.reactions_email] == delivery_type
+    return false if delivery_type.present? && User.reactions_emails[user.reactions_email] == delivery_type
 
-    logger.warn "Not sending notifications to mismatched delivery type #{delivery_type} for user #{@user.id}"
+    logger.warn "Not sending notifications to mismatched delivery type #{delivery_type} for user #{user.id}"
     true
   end
 end
--- test/workers/send_activity_notifications_worker_test.rb
+++ test/workers/send_activity_notifications_worker_test.rb
@@ -6,6 +6,7 @@ class SendActivityNotificationsWorkerTest < ActiveSupport::TestCase # rubocop:di
   define_freetown
   let!(:motion) { create(:motion, parent: freetown) }
   let!(:argument) { create(:pro_argument, parent: motion) }
+  let(:other_page_motion) { create(:motion, parent: create(:forum, url: 'other_forum', parent: create_page)) }
 
   let!(:follow) do
     create(:follow,
@@ -22,52 +23,49 @@ class SendActivityNotificationsWorkerTest < ActiveSupport::TestCase # rubocop:di
     create_notification_pair_for follower
     notification_email_mock(follower)
 
-    snw = SendActivityNotificationsWorker.new
-    snw.instance_variable_set(:@user, follower)
-    assert_equal 1, snw.send(:collect_activity_notifications).length
+    snw = worker(follower)
+    assert_equal 1, snw.send(:notifications).length
 
     email_type = User.reactions_emails[:direct_reactions_email]
-    ActsAsTenant.with_tenant(argu) { snw.perform(follower.id, email_type) }
+    snw.perform(follower.id, email_type)
 
     assert_email_sent(skip_sidekiq: true)
 
     follower.reload
-    assert_equal 0, snw.send(:collect_activity_notifications).length, 'Notifications will be send twice'
+    assert_equal 0, worker(follower).send(:notifications).length, 'Notifications will be send twice'
   end
 
   test 'should send mail to daily follower' do
     create_notification_pair_for follower_daily
     notification_email_mock(follower_daily)
 
-    snw = SendActivityNotificationsWorker.new
-    snw.instance_variable_set(:@user, follower_daily)
-    assert_equal 1, snw.send(:collect_activity_notifications).length
+    snw = worker(follower_daily)
+    assert_equal 1, snw.send(:notifications).length
 
     email_type = User.reactions_emails[:daily_reactions_email]
-    ActsAsTenant.with_tenant(argu) { snw.perform(follower_daily.id, email_type) }
+    snw.perform(follower_daily.id, email_type)
 
     assert_email_sent(skip_sidekiq: true)
 
     follower_daily.reload
-    assert_equal 0, snw.send(:collect_activity_notifications).length, 'Notifications will be send twice'
+    assert_equal 0, worker(follower_daily).send(:notifications).length, 'Notifications will be send twice'
   end
 
   test 'should send mail to weekly follower' do
     create_notification_pair_for follower_weekly
     notification_email_mock(follower_weekly)
 
-    snw = SendActivityNotificationsWorker.new
-    snw.instance_variable_set(:@user, follower_weekly)
-    assert_equal 1, snw.send(:collect_activity_notifications).length
+    snw = worker(follower_weekly)
+    assert_equal 1, snw.send(:notifications).length
 
     email_type = User.reactions_emails[:weekly_reactions_email]
-    ActsAsTenant.with_tenant(argu) { snw.perform(follower_weekly.id, email_type) }
+    snw.perform(follower_weekly.id, email_type)
 
     assert_email_sent(skip_sidekiq: true)
 
     follower_weekly.reload
     assert_equal 0,
-                 snw.send(:collect_activity_notifications).length, 'Notifications will be send twice'
+                 worker(follower_weekly).send(:notifications).length, 'Notifications will be send twice'
   end
 
   test 'should send multiple notifications as a digest' do
@@ -82,17 +80,35 @@ class SendActivityNotificationsWorkerTest < ActiveSupport::TestCase # rubocop:di
                 created_at: Time.current - 1.day
     notification_email_mock(follower)
 
-    snw = SendActivityNotificationsWorker.new
-    snw.instance_variable_set(:@user, follower)
-    assert_equal 10, snw.send(:collect_activity_notifications).length
+    snw = worker(follower)
+    assert_equal 10, snw.send(:notifications).length
 
     email_type = User.reactions_emails[:direct_reactions_email]
-    ActsAsTenant.with_tenant(argu) { snw.perform(follower.id, email_type) }
+    snw.perform(follower.id, email_type)
 
     assert_email_sent(skip_sidekiq: true)
 
     follower.reload
-    assert_equal 0, snw.send(:collect_activity_notifications).length, 'Notifications will be send twice'
+    assert_equal 0, worker(follower).send(:notifications).length, 'Notifications will be send twice'
+  end
+
+  test 'should send one email for each organization' do
+    create_notification_pair_for follower
+    create_notification_pair_for follower, resource: other_page_motion
+    notification_email_mock(follower)
+    notification_email_mock(follower, resource: other_page_motion)
+
+    snw = worker(follower)
+    assert_equal 2, snw.send(:notifications).length
+
+    email_type = User.reactions_emails[:direct_reactions_email]
+    snw.perform(follower.id, email_type)
+
+    assert_email_sent(skip_sidekiq: true, reset: false)
+    assert_email_sent(skip_sidekiq: true, root: other_page_motion.root.url)
+
+    follower.reload
+    assert_equal 0, worker(follower).send(:notifications).length, 'Notifications will be send twice'
   end
 
   test 'should not send direct mail to weekly follower' do
@@ -115,32 +131,39 @@ class SendActivityNotificationsWorkerTest < ActiveSupport::TestCase # rubocop:di
 
   private
 
-  def create_notification_pair_for(user)
+  def create_notification_pair_for(user, resource: argument)
     [Time.current, 1.day.ago].each do |created_at|
       create(:notification,
              created_at: created_at,
-             activity: argument.activities.last,
-             root_id: argument.root_id,
+             activity: resource.activities.last,
+             root_id: resource.root_id,
              user: user)
     end
 
     create(:follow,
-           followable: argument.activities.last.recipient,
+           followable: resource.activities.last.recipient,
            follower: user)
   end
 
-  def notification_email_mock(user) # rubocop:disable Metrics/MethodLength
+  def notification_email_mock(user, resource: argument) # rubocop:disable Metrics/MethodLength
+    parent = resource.parent
     create_email_mock(
       'activity_notifications',
       user.email,
       follows: [
         {
           notifications: WebMock::Matchers::AnyArgMatcher.new(false),
-          follow_id: ActsAsTenant.with_tenant(argu) { user.follow_for(motion)&.iri },
-          followable: {display_name: motion.display_name, id: motion.iri, pro: nil, type: 'Motion'},
-          organization: {display_name: motion.root.display_name}
+          follow_id: ActsAsTenant.with_tenant(resource.root) { user.follow_for(parent)&.iri },
+          followable: {display_name: parent.display_name, id: parent.iri, pro: nil, type: parent.owner_type}
         }
-      ]
+      ],
+      tenant: resource.root.url
     )
+  end
+
+  def worker(user)
+    snw = SendActivityNotificationsWorker.new
+    snw.instance_variable_set(:@user, user)
+    snw
   end
 end
